b987b47254d100f9671a51388f29dc199a27e276
Jose F Rivas C <josefelixrc7@gmail.com>
2021-07-25 14:32:52 +0000

    feat: Add `RootHandler` class

---
+++ include/Factory/root_handler.h
@@ -0,0 +1,63 @@
+/*
+ * CPW Woodpecker Server
+ * Copyright (C) 2021 CPW Online support@cpwonline.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef ROOTHANDLER_H
+#define ROOTHANDLER_H
+
+
+#include <iostream>
+
+#include <Poco/Net/HTTPRequestHandler.h>
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPResponse.h>
+#include <Poco/Net/HTTPServerResponse.h>
+#include <Poco/URI.h>
+#include <Poco/StreamCopier.h>
+#include "Poco/Data/Session.h"
+#include "Poco/Data/MySQL/Connector.h"
+#include <Poco/Data/MySQL/MySQLException.h>
+#include <Poco/Data/Statement.h>
+#include <Poco/Format.h>
+#include <Poco/NumberFormatter.h>
+#include <Poco/JSON/JSON.h>
+#include <Poco/JSON/Array.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/Parser.h>
+#include <Poco/Dynamic/Var.h>
+#include <Poco/Dynamic/Struct.h>
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace Poco::Data::Keywords;
+using Poco::Data::Session;
+using Poco::Data::Statement;
+using namespace Poco;
+using namespace Poco::Net;
+
+
+class RootHandler : public HTTPRequestHandler
+{
+	public:
+		RootHandler();
+		virtual ~RootHandler();
+};
+
+
+#endif // ROOTHANDLER_H
