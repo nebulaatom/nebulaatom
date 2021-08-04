8638690a407e8080a19aeff3947a15499f8911e8
Jose F Rivas C <josefelixrc7@gmail.com>
2021-08-04 18:59:29 +0000

    feat: Add new includes files to `HandlerFactory` class

--- include/handler_factory.h
+++ include/handler_factory.h
@@ -1,6 +1,3 @@
-#ifndef HANDLERFACTORY_H
-#define HANDLERFACTORY_H
-
 /*
  * CPW Woodpecker Server
  * Copyright (C) 2021 CPW Online support@cpwonline.org
@@ -19,14 +16,33 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef CPW_HANDLERFACTORY_H
+#define CPW_HANDLERFACTORY_H
+
+
 #include <map>
 #include <functional>
 
 #include <Poco/Net/HTTPRequestHandlerFactory.h>
+#include <Poco/JSON/JSON.h>
+#include <Poco/JSON/Array.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/Parser.h>
+#include <Poco/Dynamic/Var.h>
+#include <Poco/Dynamic/Struct.h>
+#include <Poco/StreamCopier.h>
+#include "Poco/Data/Session.h"
+#include "Poco/Data/MySQL/Connector.h"
+#include <Poco/Data/MySQL/MySQLException.h>
+#include <Poco/Data/Statement.h>
 
 #include "Factory/root_handler.h"
+#include "Factory/null_handler.h"
+#include "Factory/business_handler.h"
 
 using namespace Poco::Net;
+using namespace Poco::JSON;
+using namespace Poco::Data::Keywords;
 
 namespace CPW
 {
