1578be66aec4c835747b84331c53ff2433fecc7f
Jose F Rivas C <josefelixrc7@gmail.com>
2021-08-04 18:38:05 +0000

    feat: Add `CurrencyHandler` class

---
+++ src/Factory/currency_handler.cc
@@ -0,0 +1,54 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2021  <copyright holder> <email>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "currency_handler.h"
+
+CurrencyHandler::CurrencyHandler()
+{
+
+}
+
+CurrencyHandler::~CurrencyHandler()
+{
+
+}
+
+void CurrencyHandler::HandleDELMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void CurrencyHandler::HandlePUTMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void CurrencyHandler::HandlePOSTMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void CurrencyHandler::HandleGETMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void CurrencyHandler::handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
