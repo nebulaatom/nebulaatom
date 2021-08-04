8e2a71dd5ce24cf306a08a2910cb4030487ad8ee
Jose F Rivas C <josefelixrc7@gmail.com>
2021-08-04 18:40:25 +0000

    feat: Add `PaysHandler` class

---
+++ src/Factory/pays_handler.cc
@@ -0,0 +1,56 @@
+/*
+ * CPW Woodpecker Server
+ * Copyright (C) 2021 CPW Online support@cpwonline.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Factory/pays_handler.h"
+
+using namespace CPW::Factory;
+
+PaysHandler::PaysHandler()
+{
+
+}
+
+PaysHandler::~PaysHandler()
+{
+
+}
+
+void PaysHandler::HandleDELMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void PaysHandler::HandlePUTMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void PaysHandler::HandlePOSTMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void PaysHandler::HandleGETMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void PaysHandler::handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
