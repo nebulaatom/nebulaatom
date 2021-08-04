37ef89d6e268f3523e95370e80bf15ec292364d4
Jose F Rivas C <josefelixrc7@gmail.com>
2021-08-04 18:37:32 +0000

    feat: Add `AccountsHandler` class

---
+++ src/Factory/accounts_handler.cc
@@ -0,0 +1,56 @@
+/*
+ * CPW Woodpecker Server
+ * Copyright (C) 2021 CPW Online support@cpwonline.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Factory/accounts_handler.h"
+
+using namespace CPW::Factory;
+
+AccountsHandler::AccountsHandler()
+{
+
+}
+
+AccountsHandler::~AccountsHandler()
+{
+
+}
+
+void AccountsHandler::HandleDELMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void AccountsHandler::HandlePUTMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void AccountsHandler::HandlePOSTMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void AccountsHandler::HandleGETMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
+
+void AccountsHandler::handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response)
+{
+
+}
