e6a0a8dcdea7985a9702585d3ed247fe9dead9b1
Jose F Rivas C <josefelixrc7@gmail.com>
2021-08-16 14:17:29 +0000

    feat: Add `WebHandler` class

---
+++ include/Factory/web_handler.h
@@ -0,0 +1,44 @@
+/*
+ * CPW Woodpecker Server
+ * Copyright (C) 2021 CPW Online support@cpwonline.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CPW_FACTORY_WEBHANDLER_H
+#define CPW_FACTORY_WEBHANDLER_H
+
+#include "Factory/root_handler.h"
+
+namespace CPW {
+	namespace Factory {
+		class WebHandler;
+	}
+}
+
+class CPW::Factory::WebHandler :  CPW::Factory::RootHandler
+{
+	public:
+		WebHandler();
+		~WebHandler();
+
+	protected:
+		virtual void AddRoutes_() = 0;
+		virtual void HandleDELMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response) = 0;
+		virtual void HandlePUTMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response) = 0;
+		virtual void HandlePOSTMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response) = 0;
+		virtual void HandleGETMethod_(Poco::Net::HTTPServerRequest& request, Poco::Net::HTTPServerResponse& response) = 0;
+};
+
+#endif // CPW_FACTORY_WEBHANDLER_H
