1e253b58d36900a1c1e6a9bba26d1ce75a9e7fe6
Jose F Rivas C <josefelixrc7@gmail.com>
2021-12-04 01:01:23 +0000

    feat: Add every endpoint into `BusinessHandler` routes

--- src/Factory/business_handler.cc
+++ src/Factory/business_handler.cc
@@ -28,12 +28,46 @@ BusinessHandler::~BusinessHandler()
 void BusinessHandler::AddRoutes_()
 {
 	get_routes_list().push_back(new Route("business",std::vector<std::string>{"api", get_api_verion(), "business"}));
-	get_routes_list().push_back(new Route("users",std::vector<std::string>{"api", get_api_verion(), "business", "users"}));
-	get_routes_list().push_back(new Route("actions",std::vector<std::string>{"api", get_api_verion(), "business", "users", "actions"}));
-	get_routes_list().push_back(new Route("permissions",std::vector<std::string>{"api", get_api_verion(), "business", "permissions"}));
-	get_routes_list().push_back(new Route("permissions_log",std::vector<std::string>{"api", get_api_verion(), "business", "permissions", "permissions_log"}));
 	get_routes_list().push_back(new Route("levels",std::vector<std::string>{"api", get_api_verion(), "business", "levels"}));
-	get_routes_list().push_back(new Route("levels_log",std::vector<std::string>{"api", get_api_verion(), "business", "levels", "levels_log"}));
+	get_routes_list().push_back(new Route("levels_log",std::vector<std::string>{"api", get_api_verion(), "business", "levels", "log"}));
 	get_routes_list().push_back(new Route("banks_accounts",std::vector<std::string>{"api", get_api_verion(), "business", "banks_accounts"}));
 	get_routes_list().push_back(new Route("transactions",std::vector<std::string>{"api", get_api_verion(), "business", "banks_accounts", "transactions"}));
+
+	get_routes_list().push_back(new Route("users",std::vector<std::string>{"api", get_api_verion(), "users"}));
+	get_routes_list().push_back(new Route("settings",std::vector<std::string>{"api", get_api_verion(), "users", "settings"}));
+	get_routes_list().push_back(new Route("actions",std::vector<std::string>{"api", get_api_verion(), "users", "actions"}));
+	get_routes_list().push_back(new Route("permissions",std::vector<std::string>{"api", get_api_verion(), "users", "permissions"}));
+	get_routes_list().push_back(new Route("permissions_log",std::vector<std::string>{"api", get_api_verion(), "users", "permissions", "log"}));
+
+	get_routes_list().push_back(new Route("accounts",std::vector<std::string>{"api", get_api_verion(), "accounts"}));
+	get_routes_list().push_back(new Route("phones",std::vector<std::string>{"api", get_api_verion(), "accounts", "phones"}));
+	get_routes_list().push_back(new Route("websites",std::vector<std::string>{"api", get_api_verion(), "accounts", "websites"}));
+	get_routes_list().push_back(new Route("emails",std::vector<std::string>{"api", get_api_verion(), "accounts", "emails"}));
+	get_routes_list().push_back(new Route("addresses",std::vector<std::string>{"api", get_api_verion(), "accounts", "addresses"}));
+
+	get_routes_list().push_back(new Route("deals",std::vector<std::string>{"api", get_api_verion(), "deals"}));
+	get_routes_list().push_back(new Route("tasks",std::vector<std::string>{"api", get_api_verion(), "deals", "tasks"}));
+	get_routes_list().push_back(new Route("messages",std::vector<std::string>{"api", get_api_verion(), "deals", "messages"}));
+	get_routes_list().push_back(new Route("meetings",std::vector<std::string>{"api", get_api_verion(), "deals", "meetings"}));
+	get_routes_list().push_back(new Route("calls",std::vector<std::string>{"api", get_api_verion(), "deals", "calls"}));
+
+	get_routes_list().push_back(new Route("pays",std::vector<std::string>{"api", get_api_verion(), "pays"}));
+	get_routes_list().push_back(new Route("transactions",std::vector<std::string>{"api", get_api_verion(), "pays", "transactions"}));
+	get_routes_list().push_back(new Route("references",std::vector<std::string>{"api", get_api_verion(), "pays", "references"}));
+
+	get_routes_list().push_back(new Route("invoices",std::vector<std::string>{"api", get_api_verion(), "invoices"}));
+	get_routes_list().push_back(new Route("invoice_items",std::vector<std::string>{"api", get_api_verion(), "invoices", "items"}));
+	get_routes_list().push_back(new Route("taxes",std::vector<std::string>{"api", get_api_verion(), "invoices", "taxes"}));
+	get_routes_list().push_back(new Route("taxes_log",std::vector<std::string>{"api", get_api_verion(), "invoices", "taxes", "log"}));
+	get_routes_list().push_back(new Route("withholdings",std::vector<std::string>{"api", get_api_verion(), "invoices", "withholdings"}));
+	get_routes_list().push_back(new Route("withholdings_log",std::vector<std::string>{"api", get_api_verion(), "invoices", "withholdings", "log"}));
+
+	get_routes_list().push_back(new Route("inventories",std::vector<std::string>{"api", get_api_verion(), "inventories"}));
+	get_routes_list().push_back(new Route("inventory_items",std::vector<std::string>{"api", get_api_verion(), "inventories", "items"}));
+	get_routes_list().push_back(new Route("inventory_items_log",std::vector<std::string>{"api", get_api_verion(), "inventories", "items", "log"}));
+	get_routes_list().push_back(new Route("requirements",std::vector<std::string>{"api", get_api_verion(), "inventories", "items", "requirements"}));
+
+	get_routes_list().push_back(new Route("currency",std::vector<std::string>{"api", get_api_verion(), "currency"}));
+	get_routes_list().push_back(new Route("exchange_rate",std::vector<std::string>{"api", get_api_verion(), "currency", "exchange_rate"}));
+	get_routes_list().push_back(new Route("exchange_rate_log",std::vector<std::string>{"api", get_api_verion(), "currency", "exchange_rate", "log"}));
 }
