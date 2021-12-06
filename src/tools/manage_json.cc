442ec4a4e27856408ddf06f0c9eec75d73d82f27
Jose F Rivas C <josefelixrc7@gmail.com>
2021-12-06 21:50:37 +0000

    feat: Add `ManageJSON` class

---
+++ src/tools/manage_json.cc
@@ -0,0 +1,68 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2021  <copyright holder> <email>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tools/manage_json.h"
+
+using namespace CPW::Tools;
+
+ManageJSON::ManageJSON()
+{
+
+}
+
+ManageJSON::~ManageJSON()
+{
+
+}
+
+std::string ManageJSON::ReadBody_(std::istream& stream)
+{
+	std::string json_body;
+	StreamCopier::copyToString(stream, json_body);
+
+	if(json_body.empty())
+		return "";
+
+	return json_body;
+}
+
+bool ManageJSON::Parse_(std::string string_to_parse)
+{
+	if(string_to_parse.empty())
+		return false;
+
+	JSON::Parser parser;
+	JSON::Object::Ptr object_json = parser.parse(string_to_parse).extract<JSON::Object::Ptr>();
+	dynamic_json_body_ = *object_json;
+
+	return VerifyJSON_();
+}
+
+bool ManageJSON::VerifyJSON_()
+{
+	if
+	(
+		dynamic_json_body_["pair-information"].isEmpty()
+		|| dynamic_json_body_["pair-information"][0]["auth"].isEmpty()
+		|| dynamic_json_body_["pair-information"][1]["data"].isEmpty()
+		|| dynamic_json_body_["pair-information"][1]["data"].size() < 1
+	)
+		return false;
+	else{
+		return true;}
+}
---
+++ src/tools/manage_json.h
@@ -0,0 +1,69 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2021  <copyright holder> <email>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CPW_TOOLS_MANAGEJSON_H
+#define CPW_TOOLS_MANAGEJSON_H
+
+
+#include <istream>
+#include <string>
+
+#include <Poco/JSON/JSON.h>
+#include <Poco/JSON/JSONException.h>
+#include <Poco/JSON/Array.h>
+#include <Poco/JSON/Object.h>
+#include <Poco/JSON/Parser.h>
+#include <Poco/Dynamic/Var.h>
+#include <Poco/Dynamic/Struct.h>
+#include <Poco/StreamCopier.h>
+
+using namespace Poco;
+
+
+namespace CPW
+{
+	namespace Tools
+	{
+		class ManageJSON;
+	}
+}
+
+
+class CPW::Tools::ManageJSON
+{
+	public:
+		ManageJSON();
+		~ManageJSON();
+
+		Dynamic::Struct<std::string>& get_dynamic_json_body()
+		{
+			Dynamic::Struct<std::string>& d = dynamic_json_body_;
+			return d;
+		}
+
+		std::string ReadBody_(std::istream& stream);
+		bool Parse_(std::string string_to_parse);
+
+	protected:
+		bool VerifyJSON_();
+
+	private:
+		Dynamic::Struct<std::string> dynamic_json_body_;
+};
+
+#endif // CPW_TOOLS_MANAGEJSON_H
