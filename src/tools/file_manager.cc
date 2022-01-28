355eed4c6ed4cff68fbcd4c04964b2de428bba4e
Jose F Rivas C <josefelixrc7@gmail.com>
2022-01-28 14:52:49 +0000

    feat: Add `FileManager` class to `Tools`

---
+++ src/tools/file_manager.cc
@@ -0,0 +1,196 @@
+/*
+ * CPW Woodpecker Server
+ * Copyright (C) 2021 CPW Online support@cpwonline.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tools/file_manager.h"
+
+using namespace CPW::Tools;
+
+FileManager::FileManager() :
+	directory_base_("/srv/www")
+	,directory_for_uploaded_files_("/srv/www/uploaded-files")
+{
+	AddSupportedFiles_();
+}
+
+FileManager::~FileManager()
+{
+
+}
+
+bool FileManager::CheckFile_(Path path)
+{
+	switch(operation_type_)
+	{
+		case OperationType::kDownload:
+		case OperationType::kReplace:
+		case OperationType::kDelete:
+		{
+			requested_path_ = std::make_shared<Path>(directory_base_, Path::PATH_NATIVE);
+			requested_path_->append(path);
+			std::unique_ptr<File> tmp_file (new File(*requested_path_));
+
+			if(tmp_file->exists())
+			{
+				if(tmp_file->isDirectory())
+				{
+					requested_path_->makeDirectory();
+					requested_path_->setFileName("index.html");
+
+					tmp_file.reset(new File(*requested_path_));
+
+					if(!tmp_file->exists())
+						return false;
+				}
+			}
+			else
+				return false;
+
+			if(!tmp_file->canRead())
+				return false;
+			else
+				return true;
+			break;
+		}
+		case OperationType::kUpload:
+		{
+			DateTime now_time;
+			Random random_number;
+
+			random_number.seed();
+			std::string new_name =
+				std::to_string(now_time.year())
+				+ std::to_string(now_time.month())
+				+ std::to_string(now_time.day())
+				+ "_"
+				+ std::to_string(now_time.hour())
+				+ std::to_string(now_time.minute())
+				+ std::to_string(now_time.second())
+				+ std::to_string(now_time.millisecond())
+				+ "_"
+				+ std::to_string(random_number.next())
+			;
+
+			requested_path_ = std::make_shared<Path>(directory_for_uploaded_files_, Path::PATH_NATIVE);
+			requested_path_->append(path);
+			std::unique_ptr<File> tmp_file (new File(*requested_path_));
+
+			if(tmp_file->exists())
+				return false;
+			else
+				return true;
+		}
+	}
+}
+
+bool FileManager::IsSupported_()
+{
+	std::string extension = requested_path_->getExtension();
+	auto file_found = supported_files_.find(extension);
+	if(file_found != supported_files_.end())
+	{
+		return true;
+	}
+	else
+	{
+		for(auto it : supported_files_)
+		{
+			auto extensions = it.second.get_other_extensions();
+			auto find_depth = std::find(extensions.begin(), extensions.end(), extension);
+			if(find_depth != extensions.end())
+				return true;
+		}
+		return false;
+	}
+}
+
+void FileManager::ProcessContentType_()
+{
+	if(IsSupported_())
+		content_type_ = supported_files_.at(requested_path_->getExtension()).get_content_type();
+	else
+		content_type_ = "application/octet-stream";
+}
+
+void FileManager::ManageFile_(std::ostream& out_response)
+{
+	ProcessContentType_();
+
+	if(IsSupported_())
+	{
+		bool file_is_binary = supported_files_.at(requested_path_->getExtension()).get_binary();
+
+		if(file_is_binary)
+			ManageBinaryFile_(out_response);
+		else
+			ManageTextPlainFile_(out_response);
+	}
+	else
+	{
+		ManageBinaryFile_(out_response);
+	}
+}
+
+void FileManager::ManageBinaryFile_(std::ostream& out_response)
+{
+	std::ifstream requested_file(requested_path_->toString(), std::ios::binary | std::ios::ate);
+
+	content_length_ = requested_file.tellg();
+	std::string text_line(content_length_, '\0');
+	requested_file.seekg(0);
+	if(requested_file.read(&text_line[0], content_length_))
+	{
+		out_response << text_line;
+	}
+	requested_file.close();
+}
+
+void FileManager::ManageTextPlainFile_(std::ostream& out_response)
+{
+	std::string text_line;
+	std::ifstream requested_file(requested_path_->toString());
+	while (getline (requested_file, text_line))
+	{
+		out_response << text_line << "\n";
+	}
+	requested_file.close();
+}
+
+void FileManager::AddSupportedFiles_()
+{
+	supported_files_.emplace(std::make_pair("html", Extras::FileProperties{"text/html", false, {"htm", "html5"}}));
+	supported_files_.emplace(std::make_pair("js", Extras::FileProperties{"text/javascript", false, {"js5"}}));
+	supported_files_.emplace(std::make_pair("css",Extras::FileProperties{"text/css", false, {"css3"}}));
+	supported_files_.emplace(std::make_pair("jpeg",Extras::FileProperties{"image/jpeg", false, {"jpeg", "jpg"}}));
+	supported_files_.emplace(std::make_pair("png",Extras::FileProperties{"image/png", true, {""}}));
+	supported_files_.emplace(std::make_pair("svg",Extras::FileProperties{"image/svg+xml", true, {""}}));
+	supported_files_.emplace(std::make_pair("ttf",Extras::FileProperties{"font/ttf", true, {""}}));
+	supported_files_.emplace(std::make_pair("woff",Extras::FileProperties{"font/woff", true, {""}}));
+	supported_files_.emplace(std::make_pair("woff2",Extras::FileProperties{"font/woff2", true, {""}}));
+	supported_files_.emplace(std::make_pair("xhtml",Extras::FileProperties{"application/xhtml+xml", true, {""}}));
+	supported_files_.emplace(std::make_pair("webm",Extras::FileProperties{"video/webm", true, {""}}));
+	supported_files_.emplace(std::make_pair("xml",Extras::FileProperties{"application/xml", true, {""}}));
+	supported_files_.emplace(std::make_pair("zip",Extras::FileProperties{"application/zip", true, {""}}));
+	supported_files_.emplace(std::make_pair("wav",Extras::FileProperties{"audio/x-wav", true, {""}}));
+	supported_files_.emplace(std::make_pair("pdf",Extras::FileProperties{"application/pdf", true, {""}}));
+	supported_files_.emplace(std::make_pair("mpeg",Extras::FileProperties{"video/mpeg", true, {""}}));
+	supported_files_.emplace(std::make_pair("json",Extras::FileProperties{"application/json", true, {""}}));
+	supported_files_.emplace(std::make_pair("ico",Extras::FileProperties{"image/x-icon", true, {""}}));
+	supported_files_.emplace(std::make_pair("gif",Extras::FileProperties{"image/gif", true, {""}}));
+	supported_files_.emplace(std::make_pair("avi",Extras::FileProperties{"video/x-msvideo", true, {""}}));
+	supported_files_.emplace(std::make_pair("txt",Extras::FileProperties{"text/plain", true, {""}}));
+}
---
+++ src/tools/file_manager.h
@@ -0,0 +1,100 @@
+/*
+ * CPW Woodpecker Server
+ * Copyright (C) 2021 CPW Online support@cpwonline.org
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CPW_TOOLS_FILEMANAGER_H
+#define CPW_TOOLS_FILEMANAGER_H
+
+
+#include <string>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <memory>
+#include <sstream>
+
+#include "Poco/Path.h"
+#include "Poco/File.h"
+#include "Poco/DateTime.h"
+#include "Poco/Random.h"
+
+#include "extras/file_properties.h"
+
+
+namespace CPW
+{
+	namespace Tools
+	{
+		enum class OperationType;
+		class FileManager;
+	}
+}
+
+using namespace Poco;
+
+
+enum class CPW::Tools::OperationType
+{
+	kDownload
+	,kUpload
+	,kReplace
+	,kDelete
+};
+
+
+class CPW::Tools::FileManager
+{
+	public:
+		FileManager();
+		~FileManager();
+
+		std::shared_ptr<Path> get_requested_path() const{return requested_path_;}
+		std::map<std::string, Extras::FileProperties>& get_supported_files()
+		{
+			auto& var = supported_files_;
+			return var;
+		}
+		OperationType get_operation_type() const{return operation_type_;}
+		std::string get_directory_base() const{return directory_base_;}
+		std::string get_directory_for_uploaded_files() const{return directory_for_uploaded_files_;}
+		std::size_t get_content_length() const{return content_length_;}
+		std::string get_content_type() const{return content_type_;}
+
+		void set_operation_type(OperationType operation_type){operation_type_ = operation_type;}
+
+		bool CheckFile_(Path path);
+		bool IsSupported_();
+		void ProcessContentType_();
+		void ManageFile_(std::ostream& out_response);
+
+	protected:
+		void ManageBinaryFile_(std::ostream& out_response);
+		void ManageTextPlainFile_(std::ostream& out_response);
+
+	private:
+		void AddSupportedFiles_();
+
+		std::shared_ptr<Path> requested_path_;
+		std::map<std::string, Extras::FileProperties> supported_files_;
+		OperationType operation_type_;
+		std::string directory_base_;
+		std::string directory_for_uploaded_files_;
+		std::size_t content_length_;
+		std::string content_type_;
+};
+
+#endif // CPW_TOOLS_FILEMANAGER_H
