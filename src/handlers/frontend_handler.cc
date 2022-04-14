f27172dc34df6808fe6d2cf1ae4b02288069076b
Jose F Rivas C <josefelixrc7@gmail.com>
2022-04-14 21:56:37 +0000

    feat: Improve HandleGETMethod in FrontendHandler
    rel: #18

--- src/handlers/frontend_handler.cc
+++ src/handlers/frontend_handler.cc
@@ -39,13 +39,21 @@ void FrontendHandler::AddRoutes_()
 
 void FrontendHandler::HandleGETMethod_(HTTPServerRequest& request, HTTPServerResponse& response)
 {
+    auto tmp_file = Extras::File("file", request.getURI(), "", 0);
+    tmp_file.get_requested_path().reset(new Path(request.getURI()));
+    tmp_file.get_requested_file().reset(new Poco::File(*tmp_file.get_requested_path()));
+    auto var = tmp_file.get_requested_path()->toString();
+
+    file_manager_.get_files().push_back(tmp_file);
+
+
 	file_manager_.set_operation_type(Tools::OperationType::kDownload);
-	if(!file_manager_.CheckFile_(Path(request.getURI())))
+	if(!file_manager_.CheckFiles_())
 	{
 		GenericResponse_(response, HTTPResponse::HTTP_NOT_FOUND, "Requested file bad check.");
 		return;
 	}
-	if(!file_manager_.IsSupported_())
+	if(!file_manager_.IsSupported_(file_manager_.get_files().front()))
 	{
 		GenericResponse_(response, HTTPResponse::HTTP_BAD_REQUEST, "Requested file is not supported.");
 		return;
@@ -54,12 +62,12 @@ void FrontendHandler::HandleGETMethod_(HTTPServerRequest& request, HTTPServerRes
 	file_manager_.ProcessFileType_();
 
 	response.setStatus(HTTPResponse::HTTP_OK);
-	response.setContentType(file_manager_.get_content_type());
 
 	std::ostream& out_reponse = response.send();
-	response.setContentLength(file_manager_.get_content_length());
 
 	file_manager_.DownloadFile_(out_reponse);
+	response.setContentType(file_manager_.get_files().front().get_content_type());
+	response.setContentLength(file_manager_.get_files().front().get_content_length());
 
 	out_reponse.flush();
 }
