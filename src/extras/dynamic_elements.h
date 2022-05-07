041f22e2aa2e37d36d5203d982b81a12550489dd
Jose F Rivas C <josefelixrc7@gmail.com>
2022-05-07 19:09:25 +0000

    style: Change tabs to spaces in DynamicElements
    Rel: #28

--- src/extras/dynamic_elements.h
+++ src/extras/dynamic_elements.h
@@ -1,20 +1,20 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2021  <copyright holder> <email>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef CPW_EXTRAS_DYNAMICELEMENTS_H
 #define CPW_EXTRAS_DYNAMICELEMENTS_H
@@ -32,10 +32,10 @@
 
 namespace CPW
 {
-	namespace Extras
-	{
-		class DynamicElements;
-	}
+    namespace Extras
+    {
+        class DynamicElements;
+    }
 }
 
 using namespace Poco;
@@ -44,35 +44,35 @@ using namespace Poco::Util;
 
 class CPW::Extras::DynamicElements
 {
-	public:
-		using RouteSharedPtr = std::shared_ptr<CPW::Tools::Route>;
+    public:
+        using RouteSharedPtr = std::shared_ptr<CPW::Tools::Route>;
 
-		DynamicElements();
-		DynamicElements(DynamicElements& dynamic_elements);
-		~DynamicElements();
+        DynamicElements();
+        DynamicElements(DynamicElements& dynamic_elements);
+        ~DynamicElements();
 
-		std::list<Tools::Route>& get_routes_list()
+        std::list<Tools::Route>& get_routes_list()
         {
             auto& var = routes_list_;
             return var;
         }
-		std::shared_ptr<Core::QueryActions>& get_query_actions()
+        std::shared_ptr<Core::QueryActions>& get_query_actions()
         {
             auto& var = query_actions_;
             return var;
         }
-		Application& get_app() const {return app_;};
-		RouteSharedPtr get_requested_route() const {return requested_route_;}
+        Application& get_app() const {return app_;};
+        RouteSharedPtr get_requested_route() const {return requested_route_;}
 
-		void set_requested_route(RouteSharedPtr requested_route) {requested_route_ = requested_route;}
+        void set_requested_route(RouteSharedPtr requested_route) {requested_route_ = requested_route;}
 
-	protected:
-		RouteSharedPtr requested_route_;
+    protected:
+        RouteSharedPtr requested_route_;
 
-	private:
-		std::list<Tools::Route> routes_list_;
-		std::shared_ptr<Core::QueryActions> query_actions_;
-		Application& app_;
+    private:
+        std::list<Tools::Route> routes_list_;
+        std::shared_ptr<Core::QueryActions> query_actions_;
+        Application& app_;
 };
 
 #endif // CPW_EXTRAS_DYNAMICELEMENTS_H
