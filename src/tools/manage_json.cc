259979a95436920d6ec0d5f48d0b32582695b34c
Jose F Rivas C <josefelixrc7@gmail.com>
2022-05-07 19:23:33 +0000

    style: Change tabs to spaces in ManageJSON
    Rel: #28

--- src/tools/manage_json.cc
+++ src/tools/manage_json.cc
@@ -1,20 +1,20 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2021  <copyright holder> <email>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "tools/manage_json.h"
 
@@ -33,49 +33,49 @@ ManageJSON::~ManageJSON()
 
 std::string ManageJSON::ReadBody_(std::istream& stream)
 {
-	std::string json_body;
-	StreamCopier::copyToString(stream, json_body);
+    std::string json_body;
+    StreamCopier::copyToString(stream, json_body);
 
-	if(json_body.empty())
-		return "";
+    if(json_body.empty())
+        return "";
 
-	return json_body;
+    return json_body;
 }
 
 bool ManageJSON::Parse_(std::string string_to_parse)
 {
-	if(string_to_parse.empty())
-		return false;
+    if(string_to_parse.empty())
+        return false;
 
-	JSON::Parser parser;
+    JSON::Parser parser;
 
-	Dynamic::Var var_tmp = parser.parse(string_to_parse);
+    Dynamic::Var var_tmp = parser.parse(string_to_parse);
 
-	if(var_tmp.isArray())
-		return false;
+    if(var_tmp.isArray())
+        return false;
 
-	json_body_ = var_tmp.extract<JSON::Object::Ptr>();
+    json_body_ = var_tmp.extract<JSON::Object::Ptr>();
 
-	return VerifyJSON_();
+    return VerifyJSON_();
 }
 
 JSON::Object::Ptr ManageJSON::ExtractObject_(Dynamic::Var& object)
 {
-	if(object.isEmpty() || object.isArray())
-		return JSON::Object::Ptr();
+    if(object.isEmpty() || object.isArray())
+        return JSON::Object::Ptr();
 
-	return object.extract<JSON::Object::Ptr>();
+    return object.extract<JSON::Object::Ptr>();
 }
 
 JSON::Array::Ptr ManageJSON::ExtractArray_(Dynamic::Var object)
 {
-	auto final = JSON::Array::Ptr();
+    auto final = JSON::Array::Ptr();
 
-	if(object.isEmpty() || !object.isArray())
-		return final;
+    if(object.isEmpty() || !object.isArray())
+        return final;
 
-	final = object.extract<JSON::Array::Ptr>();
-	return final;
+    final = object.extract<JSON::Array::Ptr>();
+    return final;
 }
 
 bool ManageJSON::VerifyJSON_()
