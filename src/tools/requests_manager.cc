ac90a5e181d789ed1ed31fd56284cedd69f12f4d
Jose F Rivas C <josefelixrc7@gmail.com>
2022-05-07 19:23:47 +0000

    style: Change tabs to spaces in RequestsManager
    Rel: #28

--- src/tools/requests_manager.cc
+++ src/tools/requests_manager.cc
@@ -1,20 +1,20 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2021  <copyright holder> <email>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "tools/requests_manager.h"
 
@@ -22,7 +22,7 @@ using namespace CPW::Tools;
 
 RequestsManager::RequestsManager()
 {
-	PrepareMethods_();
+    PrepareMethods_();
 }
 
 RequestsManager::~RequestsManager()
@@ -32,24 +32,24 @@ RequestsManager::~RequestsManager()
 
 void RequestsManager::PrepareMethods_()
 {
-	actions_strings_.emplace(std::make_pair
-	(
-		"GET"
-		,[&](HTTPServerRequest& req, HTTPServerResponse& res){http_methods_->HandleGETMethod_(req, res);}
-	));
-	actions_strings_.emplace(std::make_pair
-	(
-		"POST"
-		,[&](HTTPServerRequest& req, HTTPServerResponse& res){http_methods_->HandlePOSTMethod_(req, res);}
-	));
-	actions_strings_.emplace(std::make_pair
-	(
-		"PUT"
-		,[&](HTTPServerRequest& req, HTTPServerResponse& res){http_methods_->HandlePUTMethod_(req, res);}
-	));
-	actions_strings_.emplace(std::make_pair
-	(
-		"DEL"
-		,[&](HTTPServerRequest& req, HTTPServerResponse& res){http_methods_->HandleDELMethod_(req, res);}
-	));
+    actions_strings_.emplace(std::make_pair
+    (
+        "GET"
+        ,[&](HTTPServerRequest& req, HTTPServerResponse& res){http_methods_->HandleGETMethod_(req, res);}
+    ));
+    actions_strings_.emplace(std::make_pair
+    (
+        "POST"
+        ,[&](HTTPServerRequest& req, HTTPServerResponse& res){http_methods_->HandlePOSTMethod_(req, res);}
+    ));
+    actions_strings_.emplace(std::make_pair
+    (
+        "PUT"
+        ,[&](HTTPServerRequest& req, HTTPServerResponse& res){http_methods_->HandlePUTMethod_(req, res);}
+    ));
+    actions_strings_.emplace(std::make_pair
+    (
+        "DEL"
+        ,[&](HTTPServerRequest& req, HTTPServerResponse& res){http_methods_->HandleDELMethod_(req, res);}
+    ));
 }
--- src/tools/requests_manager.h
+++ src/tools/requests_manager.h
@@ -1,20 +1,20 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2021  <copyright holder> <email>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef CPW_TOOLS_REQUESTSMANAGER_H
 #define CPW_TOOLS_REQUESTSMANAGER_H
@@ -35,10 +35,10 @@
 
 namespace CPW
 {
-	namespace Tools
-	{
-		class RequestsManager;
-	}
+    namespace Tools
+    {
+        class RequestsManager;
+    }
 }
 
 using namespace Poco;
@@ -47,29 +47,29 @@ using namespace Poco::Net;
 
 class CPW::Tools::RequestsManager
 {
-	public:
-		using MainFunctor = std::function<void(HTTPServerRequest&, HTTPServerResponse&)>;
+    public:
+        using MainFunctor = std::function<void(HTTPServerRequest&, HTTPServerResponse&)>;
 
-		RequestsManager();
-		~RequestsManager();
+        RequestsManager();
+        ~RequestsManager();
 
-		std::map<std::string, MainFunctor>& get_actions_strings()
-		{
-			auto& var = actions_strings_;
-			return var;
-		}
-		Extras::HTTPMethods* get_http_methods(){return http_methods_;}
-		void set_http_methods(Extras::HTTPMethods& http_methods)
+        std::map<std::string, MainFunctor>& get_actions_strings()
+        {
+            auto& var = actions_strings_;
+            return var;
+        }
+        Extras::HTTPMethods* get_http_methods(){return http_methods_;}
+        void set_http_methods(Extras::HTTPMethods& http_methods)
         {
             http_methods_ = &http_methods;
         }
 
-	protected:
-		void PrepareMethods_();
+    protected:
+        void PrepareMethods_();
 
-	private:
-		std::map<std::string, MainFunctor> actions_strings_;
-		Extras::HTTPMethods* http_methods_;
+    private:
+        std::map<std::string, MainFunctor> actions_strings_;
+        Extras::HTTPMethods* http_methods_;
 };
 
 #endif // CPW_TOOLS_REQUESTSMANAGER_H
