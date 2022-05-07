3ed4652b8b77f37de73a88a542bd9b02e650f392
Jose F Rivas C <josefelixrc7@gmail.com>
2022-05-07 19:22:42 +0000

    style: Change tabs to spaces in Filters
    Rel: #28

--- src/tools/filters.cc
+++ src/tools/filters.cc
@@ -1,28 +1,28 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2021  <copyright holder> <email>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "tools/filters.h"
 
 using namespace CPW::Tools;
 
 Filters::Filters() :
-	page_("0")
-	,limit_("20")
+    page_("0")
+    ,limit_("20")
 {
     MapFilterType_();
 }
--- src/tools/filters.h
+++ src/tools/filters.h
@@ -1,20 +1,20 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2021  <copyright holder> <email>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef CPW_TOOLS_FILTERS_H
 #define CPW_TOOLS_FILTERS_H
@@ -29,11 +29,11 @@
 
 namespace CPW
 {
-	namespace Tools
-	{
+    namespace Tools
+    {
         enum class FilterType;
-		class Filters;
-	}
+        class Filters;
+    }
 }
 
 
@@ -42,123 +42,123 @@ using namespace CPW::Extras;
 
 enum class CPW::Tools::FilterType
 {
-	kFields
-	,kPage
-	,kLimit
-	,kSort
-	,kIqual
-	,kNotIqual
-	,kGreatherThan
-	,kSmallerThan
-	,kBetween
-	,kIn
-	,kNotIn
-	,kValues
-	,kSet
-	,kJoins
+    kFields
+    ,kPage
+    ,kLimit
+    ,kSort
+    ,kIqual
+    ,kNotIqual
+    ,kGreatherThan
+    ,kSmallerThan
+    ,kBetween
+    ,kIn
+    ,kNotIn
+    ,kValues
+    ,kSet
+    ,kJoins
 };
 
 
 class CPW::Tools::Filters
 {
-	public:
-		using ValuesPropertiesVector = std::vector<ValuesProperties>;
-		using MapForValues = std::map<std::string, ValuesProperties>;
+    public:
+        using ValuesPropertiesVector = std::vector<ValuesProperties>;
+        using MapForValues = std::map<std::string, ValuesProperties>;
 
-		Filters();
-		~Filters();
+        Filters();
+        ~Filters();
 
-		ValuesPropertiesVector& get_fields()
-		{
-			auto& var = fields_;
-			return var;
-		}
-		std::string get_page() const { return page_; }
-		std::string get_limit() const { return limit_; }
-		ValuesPropertiesVector& get_sorts_conditions()
-		{
-			auto& var = sorts_conditions_;
-			return var;
-		}
-		MapForValues& get_iquals_conditions()
-		{
-			auto& var = iquals_conditions_;
-			return var;
-		}
-		MapForValues& get_not_iquals_conditions()
-		{
-			auto& var = not_iquals_conditions_;
-			return var;
-		}
-		MapForValues& get_greather_than()
-		{
-			auto& var = greather_than_;
-			return var;
-		}
-		MapForValues& get_smaller_than()
-		{
-			auto& var = smaller_than_;
-			return var;
-		}
-		std::map<std::string, std::pair<ValuesProperties, ValuesProperties>>& get_between()
-		{
-			auto& var = between_;
-			return var;
-		}
-		std::map<std::string, ValuesPropertiesVector>& get_in()
-		{
-			auto& var = in_;
-			return var;
-		}
-		std::map<std::string, ValuesPropertiesVector>& get_not_in()
-		{
-			auto& var = not_in_;
-			return var;
-		}
-		std::vector<ValuesPropertiesVector>& get_values()
-		{
-			auto& var = values_;
-			return var;
-		}
-		MapForValues& get_set()
-		{
-			auto& var = set_;
-			return var;
-		}
-		std::map<std::array<std::string, 2>, MapForValues>& get_joins()
-		{
-			auto& var = joins_;
-			return var;
-		}
-		std::map<std::string, Tools::FilterType>& get_filters_type_map()
+        ValuesPropertiesVector& get_fields()
+        {
+            auto& var = fields_;
+            return var;
+        }
+        std::string get_page() const { return page_; }
+        std::string get_limit() const { return limit_; }
+        ValuesPropertiesVector& get_sorts_conditions()
+        {
+            auto& var = sorts_conditions_;
+            return var;
+        }
+        MapForValues& get_iquals_conditions()
+        {
+            auto& var = iquals_conditions_;
+            return var;
+        }
+        MapForValues& get_not_iquals_conditions()
+        {
+            auto& var = not_iquals_conditions_;
+            return var;
+        }
+        MapForValues& get_greather_than()
+        {
+            auto& var = greather_than_;
+            return var;
+        }
+        MapForValues& get_smaller_than()
+        {
+            auto& var = smaller_than_;
+            return var;
+        }
+        std::map<std::string, std::pair<ValuesProperties, ValuesProperties>>& get_between()
+        {
+            auto& var = between_;
+            return var;
+        }
+        std::map<std::string, ValuesPropertiesVector>& get_in()
+        {
+            auto& var = in_;
+            return var;
+        }
+        std::map<std::string, ValuesPropertiesVector>& get_not_in()
+        {
+            auto& var = not_in_;
+            return var;
+        }
+        std::vector<ValuesPropertiesVector>& get_values()
+        {
+            auto& var = values_;
+            return var;
+        }
+        MapForValues& get_set()
+        {
+            auto& var = set_;
+            return var;
+        }
+        std::map<std::array<std::string, 2>, MapForValues>& get_joins()
+        {
+            auto& var = joins_;
+            return var;
+        }
+        std::map<std::string, Tools::FilterType>& get_filters_type_map()
         {
             auto& var = filters_type_map_;
             return var;
         }
 
-		void set_page(std::string page) { page_ = page; }
-		void set_limit(std::string limit) { limit_ = limit; }
+        void set_page(std::string page) { page_ = page; }
+        void set_limit(std::string limit) { limit_ = limit; }
 
-		bool ExistsType_(std::string type);
+        bool ExistsType_(std::string type);
 
-	private:
-		void MapFilterType_();
+    private:
+        void MapFilterType_();
 
-		ValuesPropertiesVector fields_;
-		std::string page_;
-		std::string limit_;
-		ValuesPropertiesVector sorts_conditions_;
-		MapForValues iquals_conditions_;
-		MapForValues not_iquals_conditions_;
-		MapForValues greather_than_;
-		MapForValues smaller_than_;
-		std::map<std::string, std::pair<ValuesProperties, ValuesProperties>> between_;
-		std::map<std::string, ValuesPropertiesVector> in_;
-		std::map<std::string, ValuesPropertiesVector> not_in_;
-		std::vector<ValuesPropertiesVector> values_;
-		MapForValues set_;
-		std::map<std::array<std::string, 2>, MapForValues> joins_;
-		std::map<std::string, Tools::FilterType> filters_type_map_;
+        ValuesPropertiesVector fields_;
+        std::string page_;
+        std::string limit_;
+        ValuesPropertiesVector sorts_conditions_;
+        MapForValues iquals_conditions_;
+        MapForValues not_iquals_conditions_;
+        MapForValues greather_than_;
+        MapForValues smaller_than_;
+        std::map<std::string, std::pair<ValuesProperties, ValuesProperties>> between_;
+        std::map<std::string, ValuesPropertiesVector> in_;
+        std::map<std::string, ValuesPropertiesVector> not_in_;
+        std::vector<ValuesPropertiesVector> values_;
+        MapForValues set_;
+        std::map<std::array<std::string, 2>, MapForValues> joins_;
+        std::map<std::string, Tools::FilterType> filters_type_map_;
 };
 
 
