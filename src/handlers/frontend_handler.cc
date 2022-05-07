33dd3c44023043fe55e631808a3559406078828f
Jose F Rivas C <josefelixrc7@gmail.com>
2022-05-07 19:20:30 +0000

    style: Change tabs to spaces in BackendHandler
    Rel: #28

--- src/handlers/frontend_handler.cc
+++ src/handlers/frontend_handler.cc
@@ -1,27 +1,27 @@
 /*
- * CPW Woodpecker Server
- * Copyright (C) 2021 CPW Online support@cpwonline.org
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* CPW Woodpecker Server
+* Copyright (C) 2021 CPW Online support@cpwonline.org
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "handlers/frontend_handler.h"
 
 using namespace CPW::Handlers;
 
 FrontendHandler::FrontendHandler(std::string api_version) :
-	RootHandler(api_version)
+    RootHandler(api_version)
 {
 
 }
@@ -33,8 +33,8 @@ FrontendHandler::~FrontendHandler()
 
 void FrontendHandler::AddRoutes_()
 {
-	get_dynamic_elements()->get_routes_list().push_back({"",{""}});
-	get_dynamic_elements()->get_routes_list().push_back({"uploaded-files",{"uploaded-files"}});
+    get_dynamic_elements()->get_routes_list().push_back({"",{""}});
+    get_dynamic_elements()->get_routes_list().push_back({"uploaded-files",{"uploaded-files"}});
 }
 
 void FrontendHandler::HandleGETMethod_(HTTPServerRequest& request, HTTPServerResponse& response)
@@ -46,7 +46,7 @@ void FrontendHandler::HandleGETMethod_(HTTPServerRequest& request, HTTPServerRes
 
         file_manager_.get_files().push_back(tmp_file);
 
-	// Basic operations
+    // Basic operations
         file_manager_.set_operation_type(Tools::OperationType::kDownload);
         if(!file_manager_.CheckFiles_())
         {
@@ -71,7 +71,7 @@ void FrontendHandler::HandleGETMethod_(HTTPServerRequest& request, HTTPServerRes
     // Download file
         file_manager_.DownloadFile_(out_reponse);
 
-	out_reponse.flush();
+    out_reponse.flush();
 }
 
 void FrontendHandler::HandlePOSTMethod_(HTTPServerRequest& request, HTTPServerResponse& response)
@@ -80,7 +80,7 @@ void FrontendHandler::HandlePOSTMethod_(HTTPServerRequest& request, HTTPServerRe
         file_manager_.set_operation_type(Tools::OperationType::kUpload);
         HTMLForm form(request, request.stream(), file_manager_);
 
-	// Basic operations
+    // Basic operations
         for(auto& file_it : file_manager_.get_files())
         {
             if(!file_manager_.IsSupported_(file_it))
