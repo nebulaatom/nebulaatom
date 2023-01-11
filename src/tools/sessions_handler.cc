d509d0d7ddaf6c0de95b690b3f304ce866bb3b89
Jose F Rivas C <josefelixrc7@gmail.com>
2023-01-10 21:38:49 -0400

    feat: Add SessionHandler class to Tools
    This class will enable to manage some sessions in a
    map and perform some sessions operations.
    
    Rel: #42

---
+++ src/tools/sessions_handler.cc
@@ -0,0 +1,64 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2023  Jose F Rivas C <email>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tools/sessions_handler.h"
+
+using namespace CPW::Tools;
+
+SessionsHandler::SessionsHandler()
+{
+
+}
+
+SessionsHandler::~SessionsHandler()
+{
+
+}
+
+CPW::Extras::Session& SessionsHandler::CreateSession_(std::string user, std::string path, bool expire, int max_age)
+{
+    Extras::Session new_session;
+    new_session.set_user(user);
+    new_session.set_path(path);
+    new_session.set_expire(expire);
+    new_session.set_max_age(max_age);
+
+    std::string id;
+    while(true)
+    {
+        id = new_session.get_id();
+        if(SessionExists_(id))
+            new_session.GenerateNewSessionID_();
+        else
+            break;
+    }
+
+    sessions_.emplace(id, std::move(new_session));
+    return sessions_.at(id);
+}
+
+void SessionsHandler::DeleteSession_(std::string id)
+{
+    if(sessions_.find(id) != sessions_.end())
+        sessions_.erase(id);
+}
+
+bool SessionsHandler::SessionExists_(std::string id)
+{
+    return sessions_.find(id) != sessions_.end();
+}
---
+++ src/tools/sessions_handler.h
@@ -0,0 +1,59 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2023  Jose F Rivas C <email>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CPW_HTTP_SESSIONS_HANDLER_H
+#define CPW_HTTP_SESSIONS_HANDLER_H
+
+
+#include <string>
+#include <map>
+
+#include "extras/session.h"
+
+namespace CPW
+{
+    namespace Tools
+    {
+        class SessionsHandler;
+    }
+}
+
+
+class CPW::Tools::SessionsHandler
+{
+    public:
+        SessionsHandler();
+        ~SessionsHandler();
+
+        std::map<std::string, CPW::Extras::Session>& get_sessions()
+        {
+            auto& var = sessions_;
+            return var;
+        }
+
+        CPW::Extras::Session& CreateSession_(std::string user, std::string path, bool expire, int max_age);
+        void DeleteSession_(std::string id);
+
+    protected:
+        bool SessionExists_(std::string id);
+
+    private:
+        std::map<std::string, CPW::Extras::Session> sessions_;
+};
+
+#endif // CPW_HTTP_SESSIONS_HANDLER_H
