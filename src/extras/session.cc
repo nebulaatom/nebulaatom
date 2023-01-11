3575efe9e5830379ee2ca5fa9a0f2fbc8bb878fc
Jose F Rivas C <josefelixrc7@gmail.com>
2023-01-10 21:37:42 -0400

    feat: Add Session class to Extras
    This class will enable manage users sessions.
    
    Rel: #42

---
+++ src/extras/session.cc
@@ -0,0 +1,64 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2023  Jose F Rivas C <email>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "extras/session.h"
+
+using namespace CPW::Extras;
+
+Session::Session() :
+        id_("")
+        ,path_("/")
+        ,user_("")
+        ,expire_(false)
+        ,max_age_(3600)
+        ,last_access_()
+{
+    GenerateSessionID_();
+}
+
+Session::~Session()
+{
+
+}
+
+void Session::GenerateNewSessionID_()
+{
+    GenerateSessionID_();
+}
+
+void Session::GenerateSessionID_()
+{
+    for(int a = 0; a < 32; a++)
+    {
+        Random random;
+        random.seed();
+        id_.push_back(random.nextChar());
+    }
+
+    GenerateMD5Hash_();
+}
+
+void Session::GenerateMD5Hash_()
+{
+	MD5Engine md5;
+	DigestOutputStream ostr(md5);
+	ostr << id_;
+	ostr.flush();
+	const DigestEngine::Digest& digest = md5.digest();
+	id_ = DigestEngine::digestToHex(digest);
+}
---
+++ src/extras/session.h
@@ -0,0 +1,109 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2023  Jose F Rivas C <email>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CPW_EXTRAS_SESSION_H
+#define CPW_EXTRAS_SESSION_H
+
+
+#include <string>
+
+#include "Poco/Random.h"
+#include <Poco/Net/HTTPCookie.h>
+#include <Poco/DateTime.h>
+#include "Poco/DigestStream.h"
+#include "Poco/MD5Engine.h"
+
+
+namespace CPW
+{
+    namespace Extras
+    {
+        class Session;
+    }
+}
+
+using namespace Poco;
+using namespace Poco::Net;
+
+
+class CPW::Extras::Session
+{
+    public:
+        Session();
+        virtual ~Session();
+
+        std::string get_id() const
+        {
+            return id_;
+        }
+        std::string get_path() const
+        {
+            return path_;
+        }
+        std::string get_user() const
+        {
+            return user_;
+        }
+        bool get_expire() const
+        {
+            return expire_;
+        }
+        int get_max_age() const
+        {
+            return max_age_;
+        }
+        Poco::DateTime get_last_access() const
+        {
+            return last_access_;
+        }
+
+        void set_path(std::string path)
+        {
+            path_ = path;
+        }
+        void set_user(std::string user)
+        {
+            user_ = user;
+        }
+        void set_expire(bool expire)
+        {
+            expire_ = expire;
+        }
+        void set_max_age(int max_age)
+        {
+            max_age_ = max_age;
+        }
+
+        void GenerateNewSessionID_();
+
+    protected:
+        void GenerateSessionID_();
+        void GenerateMD5Hash_();
+
+    private:
+        std::string id_;
+        std::string path_;
+        std::string user_;
+        bool expire_;
+        int max_age_;
+        Poco::DateTime last_access_;
+
+};
+
+
+#endif // CPW_EXTRAS_SESSION_H
