3e8f939d278effde5ea385b9316d6d07c6881b2f
Jose F Rivas C <josefelixrc7@gmail.com>
2023-06-03 16:15:38 -0400

    feat: Add Permission class
    Rel: #78, #85

---
+++ src/security/permission.cpp
@@ -0,0 +1,41 @@
+/*
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "security/permission.h"
+
+using namespace CPW::Security;
+
+Permission::Permission() :
+    granted_(false)
+    ,descendant_(false)
+    ,route_({})
+    ,user_({})
+    ,action_type_(ActionType::kRead)
+{
+
+}
+
+Permission::Permission(bool granted, Tools::Route route, std::shared_ptr<User> user, ActionType action_type, bool descendant = false) :
+    granted_(granted)
+    ,descendant_(descendant)
+    ,route_(route)
+    ,user_(user)
+    ,action_type_(action_type)
+{
+
+}
