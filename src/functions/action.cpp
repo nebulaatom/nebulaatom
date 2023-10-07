7dfbd6299d52202d55fab5760f47543fe128fa5f
Jose F Rivas C <josefelixrc7@gmail.com>
2023-10-07 13:42:51 -0400

    feat: Add Functions::Action
    Rel: #106, #110

---
+++ src/functions/action.cpp
@@ -0,0 +1,11 @@
+
+#include "functions/action.h"
+
+using namespace CPW::Functions;
+
+Action::Action() :
+    custom_error_("")
+    ,sql_code_("")
+{
+
+}
---
+++ src/functions/action.h
@@ -0,0 +1,54 @@
+
+#ifndef FUNCTIONS_ACTION_H
+#define FUNCTIONS_ACTION_H
+
+
+#include <string>
+#include <vector>
+
+#include "tools/row_value_formatter.h"
+#include "query/results.h"
+#include "query/parameter.h"
+
+
+namespace CPW
+{
+    namespace Functions
+    {
+        class Action;
+    }
+}
+
+
+class CPW::Functions::Action
+{
+    public:
+        Action();
+
+        std::string get_identifier() const { return identifier_; };
+        std::string get_custom_error() const { return custom_error_; };
+        std::vector<Query::Parameter>& get_parameters()
+        {
+            auto& var = parameters_;
+            return var;
+        }
+        std::string get_sql_code() const { return sql_code_; };
+        Query::Results& get_results()
+        {
+            auto& var = results_;
+            return var;
+        }
+
+        void set_identifier(std::string identifier) { identifier_ = identifier; };
+        void set_custom_error(std::string custom_error) { custom_error_ = custom_error; };
+        void set_sql_code(std::string sql_code) { sql_code_ = sql_code; };
+
+    private:
+        std::string identifier_;
+        std::string custom_error_;
+        std::vector<Query::Parameter> parameters_;
+        std::string sql_code_;
+        Query::Results results_;
+};
+
+#endif // FUNCTIONS_ACTION_H
