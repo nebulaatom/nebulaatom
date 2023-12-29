3c6107617dc07ee67c3e2c2dbb412aac6639b231
Jose F Rivas C <josefelixrc7@gmail.com>
2023-12-29 15:11:40 -0400

    feat: Add constructor to Function class
    Rel: #74, #113

--- src/functions/function.cpp
+++ src/functions/function.cpp
@@ -4,6 +4,18 @@
 using namespace CPW;
 using namespace CPW::Functions;
 
+Function::Function() :
+    endpoint_("")
+    ,endpoint2_("")
+    ,target_("")
+    ,type_(Type::kGET)
+{
+    methods_.insert({"POST", Type::kPOST});
+    methods_.insert({"GET", Type::kGET});
+    methods_.insert({"PUT", Type::kPUT});
+    methods_.insert({"DEL", Type::kDEL});
+}
+
 Function::Function(std::string endpoint, Type type) :
     endpoint_(endpoint)
     ,endpoint2_("")
