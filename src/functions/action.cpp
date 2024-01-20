e80676d655729dacc18b3fe7113dc1ace0be7ac6
Jose F Rivas C <josefelixrc7@gmail.com>
2024-01-20 17:15:50 -0400

    feat: Make base class to Action class
    Rel: #63

--- src/functions/action.cpp
+++ src/functions/action.cpp
@@ -1,18 +1,133 @@
 
 #include "functions/action.h"
-#include "query/results.h"
-#include <memory>
+#include "Poco/Util/Application.h"
 
 using namespace CPW::Functions;
 
 Action::Action(std::string identifier) :
-    identifier_(identifier)
+    app_(Application::instance())
+    ,action_type_(ActionType::kSQL)
+    ,identifier_(identifier)
     ,status_("OK.")
     ,message_("OK.")
     ,custom_error_("")
     ,final_(false)
     ,error_(false)
-    ,sql_code_("SELECT 1")
 {
     results_ = std::make_shared<Query::Results>();
+}
+
+Action::~Action()
+{
+
+}
+
+
+void Action::IdentifyParameters_()
+{
+    try
+    {
+        auto& data_array = get_json_body();
+
+        for (std::size_t a = 0; a < data_array->size(); a++)
+        {
+            // Get the action object
+                if(data_array->get(a).isEmpty())
+                {
+                    app_.logger().error("Data array haves an empty action.");
+                    continue;
+                }
+
+                auto action_object = data_array->getObject(a);
+
+            // Get the action identifier
+                if(action_object->get("action_id").isEmpty() || !action_object->get("action_id").isString())
+                {
+                    app_.logger().error("The action object does not have an action_id String Object.");
+                    continue;
+                }
+
+                auto action_id = action_object->get("action_id").toString();
+
+            // Verify Action identifier
+                if(action_id != get_identifier())
+                    continue;
+
+            // Get the parameters object
+                if(action_object->get("parameters").isEmpty() || !action_object->get("parameters").isArray())
+                {
+                    app_.logger().error("The action object does not have a parameters array.");
+                    continue;
+                }
+
+                auto parameters_array = action_object->getArray("parameters");
+
+            // Iterate over parameters array
+                for(std::size_t b = 0; b < parameters_array->size(); b++)
+                {
+                    // Get parameter object
+                    if(parameters_array->get(b).isEmpty())
+                    {
+                        app_.logger().error("Parameters array haves an empty element.");
+                        continue;
+                    }
+
+                    auto parameter_object = parameters_array->getObject(b);
+
+                    // Get parameter name
+                    if(parameter_object->get("name").isEmpty() || !parameter_object->get("name").isString())
+                    {
+                        app_.logger().error("Parameter name is not a String Object.");
+                        continue;
+                    }
+
+                    auto parameter_name = parameter_object->get("name").toString();
+
+                    // Get parameter value
+                    if(parameter_object->get("value").isEmpty())
+                    {
+                        app_.logger().error("Parameter value is empty.");
+                        continue;
+                    }
+
+                    auto parameter_value = parameter_object->get("value");
+
+                    // Find if exists parameter
+                    auto found_param = std::find_if(get_parameters().begin(), get_parameters().end(), [parameter_name](Query::Parameter parameter)
+                    {
+                        return parameter.get_name() == parameter_name;
+                    });
+
+                    // Remplace parameter value
+                    if(found_param != get_parameters().end())
+                    {
+                        if(found_param->get_editable())
+                        {
+                            auto index = std::distance(get_parameters().begin(), found_param);
+                            get_parameters().erase(found_param);
+
+                            // Insert new element
+                            auto parameter = Query::Parameter(parameter_name, parameter_value, true);
+                            get_parameters().insert(get_parameters().begin() + index, std::move(parameter));
+                        }
+                    }
+
+                }
+
+        }
+    }
+    catch(std::runtime_error& error)
+    {
+        std::string string_error = "- Error on query_actions.cc on IdentifyFilters_(): " + std::string(error.what());
+        app_.logger().error(string_error);
+        set_error(true);
+        set_custom_error(string_error);
+    }
+    catch(JSON::JSONException& error)
+    {
+        std::string string_error = "- Error on query_actions.cc on IdentifyFilters_(): " + std::string(error.what());
+        app_.logger().error(string_error);
+        set_error(true);
+        set_custom_error(string_error);
+    }
 }
