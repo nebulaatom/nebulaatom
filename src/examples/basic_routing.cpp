808a579ea4842547eba11b5243c2885a1b84e573
Jose F Rivas C <josefelixrc7@gmail.com>
2024-02-26 12:36:29 -0400

    feat: Add BasicRouting executable
    Rel: #116, #117

--- src/examples/basic_routing.cpp
+++ src/examples/basic_routing.cpp
@@ -1,15 +1,42 @@
 
 #include "core/handler_factory.h"
 #include "core/woodpecker_server.h"
+#include "handlers/null_handler.h"
 #include "handlers/root_handler.h"
 
 using namespace CPW;
 
-class HelloHandler : public Handlers::RootHandler
+class HTMLHandler : public Handlers::RootHandler
 {
     public:
-        HelloHandler() : Handlers::RootHandler("v1"){}
-        virtual ~HelloHandler() {}
+        HTMLHandler() : Handlers::RootHandler("v1"){}
+        virtual ~HTMLHandler() {}
+
+        void Process_() override
+        {
+            try
+            {
+                // Say Hello
+                    HTMLResponse_(*get_response(), HTTPResponse::HTTP_OK, "Hello!");
+            }
+            catch(std::exception& error)
+            {
+                app_.logger().error("- Error on hello_world.cpp on Process_(): " + std::string(error.what()));
+                GenericResponse_(*get_response(), HTTPResponse::HTTP_INTERNAL_SERVER_ERROR, "Internal server error. " + std::string(error.what()));
+            }
+            catch(...)
+            {
+                app_.logger().error("- Error on hello_world.cpp on Process_(): No handled exception.");
+                GenericResponse_(*get_response(), HTTPResponse::HTTP_INTERNAL_SERVER_ERROR, "Internal server error. No handled exception." );
+            }
+        }
+};
+
+class JSONHandler : public Handlers::RootHandler
+{
+    public:
+        JSONHandler() : Handlers::RootHandler("v1"){}
+        virtual ~JSONHandler() {}
 
         void Process_() override
         {
@@ -39,8 +66,23 @@ int main(int argc, char** argv)
 
         auto hello_handler = new Core::HandlerFactory::FunctionRequest([&](const HTTPServerRequest& request)
         {
-            return new HelloHandler;
+            CPW::Handlers::RootHandler* handler = nullptr;
+            if(request.getURI() == "/json")
+            {
+                handler = new JSONHandler;
+            }
+            else if(request.getURI() == "/html")
+            {
+                handler = new HTMLHandler;
+            }
+            else
+            {
+                handler = new Handlers::NullHandler("");
+            }
+
+            return handler;
         });
+
         auto& request_handler_creator = app.get_handler_factory()->get_request_handler_creator();
         request_handler_creator.reset(hello_handler);
 
