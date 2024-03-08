8ae33f07f227c5109c334309f7392f2d33fa0501
Jose F Rivas C <josefelixrc7@gmail.com>
2024-03-08 18:17:36 -0400

    feat: Add CustomHandler class
    Rel: #116, #117

---
+++ src/handlers/custom_handler.h
@@ -0,0 +1,63 @@
+/*
+* CPW Woodpecker Server
+* Copyright (C) 2021 CPW Online support@cpwonline.org
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CPW_HANDLERS_CUSTOM_HANDLER_H
+#define CPW_HANDLERS_CUSTOM_HANDLER_H
+
+
+#include "handlers/root_handler.h"
+
+
+namespace CPW
+{
+    namespace Handlers
+    {
+        class CustomHandler;
+    }
+}
+
+
+class CPW::Handlers::CustomHandler :
+    public RootHandler
+{
+    public:
+        CustomHandler();
+        CustomHandler(std::function<void()> process_function);
+        virtual ~CustomHandler();
+
+        std::function<void()>& get_process_function()
+        {
+            auto& var = process_function_;
+            return var;
+        }
+
+        void SetProcessFunction(std::function<void()> process_function);
+
+    protected:
+        virtual void AddFunctions_() override;
+        virtual void Process_() override;
+        virtual void HandleGETMethod_() override;
+        virtual void HandlePOSTMethod_() override;
+        virtual void HandlePUTMethod_() override;
+        virtual void HandleDELMethod_() override;
+
+    private:
+        std::function<void()> process_function_;
+};
+
+#endif // CPW_HANDLERS_CUSTOM_HANDLER_H
