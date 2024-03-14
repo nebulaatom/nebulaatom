5c4a614a00e7ad12fc37570af64d5c18d305eaaf
Jose F Rivas C <josefelixrc7@gmail.com>
2024-03-14 12:49:15 -0400

    feat: Add CustomError example
    Rel: #116, #117

---
+++ src/examples/custom_error.cpp
@@ -0,0 +1,24 @@
+
+#include "core/nebula_atom.h"
+#include "handlers/custom_handler.h"
+#include "http/common_responses.h"
+
+using namespace Atom;
+
+int main(int argc, char** argv)
+{
+    Core::NebulaAtom app;
+
+    app.CustomHandlerCreator_([&](const HTTPServerRequest& request)
+    {
+        return new Handlers::CustomHandler([&](Handlers::CustomHandler& self)
+        {
+            if(request.getURI() == "/hello")
+                self.CustomHTMLResponse_(HTTP::Status::kOK, "Hello! Custom error");
+            else
+                self.FileResponse_(HTTP::Status::kHTTP_NOT_FOUND, "/var/www/404.html");
+        });
+    });
+
+    return app.run(argc, argv);
+}
