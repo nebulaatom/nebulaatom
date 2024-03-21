bfb2e3f4fd6da1fa0f907889e73878fe1a490426
Jose F Rivas C <josefelixrc7@gmail.com>
2024-03-21 15:37:17 -0400

    chore: Improve CMake version
    Rel: #123

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -22,4 +22,12 @@ endif()
 # Setting up variables
 set(PACKAGE_VERSION "${VERSION}")
 set(PACKAGE_VERSION_COMPLETE "${VERSION}")
+
+# Extract
+string(REGEX MATCH "v([0-9]+)\\.([0-9]+)\\.([0-9]+)" _ ${VERSION})
+
+set(${PROJECT_NAME}_VERSION_MAJOR ${CMAKE_MATCH_1})
+set(${PROJECT_NAME}_VERSION_PATCH ${CMAKE_MATCH_2})
+set(${PROJECT_NAME}_VERSION_MINOR ${CMAKE_MATCH_3})
+
 message(STATUS "Package version: ${PACKAGE_VERSION}")
