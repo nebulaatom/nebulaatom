a1fb5628af97515ab7c05d2e46325ece19dec63b
Jose F Rivas C <josefelixrc7@gmail.com>
2024-03-21 09:29:13 -0400

    chore: Add version and update version CMake
    Rel: #114

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,13 +1,25 @@
-# Get Git tag
+
+# Read current version
+file(READ ${CMAKE_SOURCE_DIR}/version.txt CURRENT_VERSION)
+set(VERSION ${CURRENT_VERSION})
+
+# Get version from Git
 execute_process(
     COMMAND git describe --tags --abbrev=0
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    OUTPUT_VARIABLE VERSION
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    OUTPUT_VARIABLE NEW_VERSION
     OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-# Set variables
+# Verify new Git version
+if(NOT NEW_VERSION STREQUAL "")
+    if(NOT NEW_VERSION STREQUAL CURRENT_VERSION)
+        set(VERSION ${NEW_VERSION})
+        file(WRITE ${CMAKE_SOURCE_DIR}/version.txt ${NEW_VERSION})
+    endif()
+endif()
+
+# Setting up variables
 set(PACKAGE_VERSION "${VERSION}")
 set(PACKAGE_VERSION_COMPLETE "${VERSION}")
-
 message(STATUS "Package version: ${PACKAGE_VERSION}")
