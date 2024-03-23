5d7049cd0c028e0e901f393427c706b21ed7b995
Jose F Rivas C <josefelixrc7@gmail.com>
2024-03-23 17:18:46 -0400

    chore: Fix dependencies to CPack
    Rel: #123

--- cmake/cpack.cmake
+++ cmake/cpack.cmake
@@ -1,7 +1,7 @@
 # do this after all install(...) commands so that all targets are finalized. 
 # Essentially, the last thing included at the end of the top-level CMakeLists.txt
 
-set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
+set(CPACK_PACKAGE_NAME libnebulaatom)
 set(CPACK_PACKAGE_VENDOR "NebulaCSF")
 set(CPACK_GENERATOR "DEB")
 set(CPACK_SOURCE_GENERATOR "DEB")
@@ -18,7 +18,7 @@ set(CPACK_PACKAGE_VERSION_MAJOR "${${PROJECT_NAME}_VERSION_MAJOR}")
 set(CPACK_PACKAGE_VERSION_PATCH "${${PROJECT_NAME}_VERSION_PATCH}")
 set(CPACK_PACKAGE_VERSION_MINOR "${${PROJECT_NAME}_VERSION_MINOR}")
 
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "poco (>= 1.11.0), yaml-cpp (>= 0.7.0)")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libpoco-dev (>= 1.10.0), libyaml-cpp-dev (>= 0.6.3), libssl-dev (>= 1.1.1), libmariadb-dev (>= 10.5.23)")
 set(CPACK_PACKAGE_DESCRIPTION "C++ Web Framework for building high-performance APIs, supporting scalable architecture and modern web technologies")
 set(CPACK_PACKAGE_CONTACT "Jose Rivas josefelixrc7@gmail.com")
 
