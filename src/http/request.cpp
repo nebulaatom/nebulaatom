e5116effb9256d8e3427ad8c4f00a67f22e10a52
Jose F Rivas C <josefelixrc7@gmail.com>
2024-04-11 18:03:20 -0400

    feat: Add Request class
    Rel: #41

---
+++ src/http/request.cpp
@@ -0,0 +1,67 @@
+/*
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "http/request.h"
+#include <optional>
+#include <stdexcept>
+
+using namespace Atom::HTTP;
+
+Request::Request(HTTP::RequestType request_type) :
+    request_type_(request_type)
+{
+    http_server_request_ = std::nullopt;
+    http_server_response_ = std::nullopt;
+}
+
+void Request::AddHeader_(std::string name, std::string value)
+{
+    headers_.push_back(Request::Header(name, value));
+}
+
+void Request::SetupResponseHeaders_()
+{
+    for(auto header : headers_)
+    {
+        if(http_server_response_.has_value())
+        {
+            http_server_response_.value()->set(header.name, header.value);
+        }
+        else
+            throw std::runtime_error("HTTPServerRequest or HTTPServerResponse cannot be null");
+    }
+}
+
+void Request::SetupConstRequest_(const Net::HTTPServerRequest& request)
+{
+    http_server_const_request_.emplace(&request);
+    uri_ = request.getURI();
+    method_ = request.getMethod();
+}
+
+void Request::SetupRequest_(Net::HTTPServerRequest& request)
+{
+    http_server_request_.emplace(&request);
+    uri_ = request.getURI();
+    method_ = request.getMethod();
+}
+
+void Request::SetupResponse_(Net::HTTPServerResponse& response)
+{
+    http_server_response_.emplace(&response);
+}
---
+++ src/http/request.h
@@ -0,0 +1,115 @@
+/*
+* <one line to give the program's name and a brief idea of what it does.>
+* Copyright (C) 2021  <copyright holder> <email>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef ATOM_HTTP_REQUEST
+#define ATOM_HTTP_REQUEST
+
+
+#include <vector>
+#include <optional>
+
+#include <Poco/Net/HTTPServerRequest.h>
+#include <Poco/Net/HTTPServerResponse.h>
+
+
+namespace Atom
+{
+    namespace HTTP
+    {
+        enum class RequestType;
+        class Request;
+    }
+}
+
+using namespace Poco;
+using namespace Poco::Net;
+
+
+enum class Atom::HTTP::RequestType
+{
+    kRequest
+    ,kConstRequest
+};
+
+class Atom::HTTP::Request
+{
+    public:
+        struct Header
+        {
+            Header(std::string name, std::string value) : 
+                name(name)
+                ,value(value)
+            {}
+
+            std::string name, value;
+        };
+
+        using HTTPServerConstRequestPtr = std::optional<const HTTPServerRequest*>;
+        using HTTPServerRequestPtr = std::optional<HTTPServerRequest*>;
+        using HTTPServerResponsePtr = std::optional<HTTPServerResponse*>;
+
+        Request(RequestType request_type);
+        
+        RequestType get_request_type() const { return request_type_; }
+        HTTPServerConstRequestPtr& get_http_server_const_request()
+        {
+            auto& var = http_server_const_request_;
+            return var;
+        }
+        HTTPServerRequestPtr& get_http_server_request()
+        {
+            auto& var = http_server_request_;
+            return var;
+        }
+        HTTPServerResponsePtr& get_http_server_response()
+        {
+            auto& var = http_server_response_;
+            return var;
+        }
+        std::vector<Request::Header>& get_headers()
+        {
+            auto& var = headers_;
+            return var;
+        }
+        std::string get_uri() const { return uri_; }
+        std::string get_method() const { return method_; }
+
+        void set_request_type(RequestType request_type) { request_type_ = request_type; }
+        void set_uri(std::string uri) { uri_ = uri; }
+        void set_method(std::string method) { method_ = method; }
+        
+        void AddHeader_(std::string name, std::string value);
+        void SetupResponseHeaders_();
+
+    protected:
+        void SetupConstRequest_(const Net::HTTPServerRequest& request);
+        void SetupRequest_(Net::HTTPServerRequest& request);
+        void SetupResponse_(Net::HTTPServerResponse& response);
+
+    private:
+        RequestType request_type_;
+        HTTPServerConstRequestPtr http_server_const_request_;
+        HTTPServerRequestPtr http_server_request_;
+        HTTPServerResponsePtr http_server_response_;
+        std::vector<Request::Header> headers_;
+        std::string uri_;
+        std::string method_;
+
+};
+
+#endif // ATOM_HTTP_REQUEST
