6adffda393a2b7033a0e199b378d66b694a3676c
Jose F Rivas C <josefelixrc7@gmail.com>
2024-04-23 12:40:22 -0400

    feat: Add SSL connectivity
    Rel: #135

--- src/http/client.cpp
+++ src/http/client.cpp
@@ -17,28 +17,64 @@
 */
 
 #include "http/client.h"
+#include <Poco/Net/NetSSL.h>
+#include <Poco/Net/SSLException.h>
 
 using namespace Atom::HTTP;
 
-Client::Client(std::string uri) :
-    uri_(uri)
+Client::Client(std::string uri, const std::string method) :
+    use_ssl_(false)
+    ,uri_(uri)
+    ,method_(method)
     ,username_("")
     ,password_("")
     ,use_credentials_(false)
+    ,ssl_context_(nullptr)
 {
     
 }
 
-Client::Client(std::string uri, std::string username, std::string password) :
-    uri_(uri)
-    ,username_(username)
-    ,password_(password)
-    ,use_credentials_(true)
+void Client::UseCredentials_(std::string username, std::string password)
+{
+    username_ = username;
+    password_ = password;
+}
+
+void Client::SetupSSL_(std::string key, std::string cert)
+{
+    use_ssl_ = true;
+    /*Net::initializeSSL();
+    SharedPtr<Net::InvalidCertificateHandler> cert_handler = new AcceptCertificateHandler(false);*/
+    ssl_context_ = new Context(Context::CLIENT_USE, key, cert, "", Context::VERIFY_RELAXED, 9, true, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+    //SSLManager::instance().initializeClient(0, cert_handler, ssl_context_);
+}
+
+void Client::SetupSSL_(std::string rootcert)
+{
+    use_ssl_ = true;
+    /*Net::initializeSSL();
+    SharedPtr<Net::InvalidCertificateHandler> cert_handler = new AcceptCertificateHandler(false);*/
+    ssl_context_ = new Context(Context::CLIENT_USE, "", "", rootcert, Context::VERIFY_RELAXED, 9, true, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+    //SSLManager::instance().initializeClient(0, cert_handler, ssl_context_);
+}
+
+void Client::AddHeader_(std::string name, std::string value)
+{
+    
+}
+
+void Client::AddCookie_(std::string name, std::string value)
 {
     
 }
 
 void Client::SendRequest_()
+{
+    if(use_ssl_) SendSSLRequest_();
+    else SendNormalRequest_();
+}
+
+void Client::SendNormalRequest_()
 {
 	try
 	{
@@ -47,7 +83,7 @@ void Client::SendRequest_()
 		if (path.empty()) path = "/";
 
 		Net::HTTPClientSession session(uri.getHost(), uri.getPort());
-		Net::HTTPRequest http_request(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+		Net::HTTPRequest http_request(method_, path, HTTPMessage::HTTP_1_1);
 		Net::HTTPResponse http_response;
 
         if(use_credentials_)
@@ -61,6 +97,45 @@ void Client::SendRequest_()
         std::stringstream ss;
 		StreamCopier::copyStream(is, ss);
         response_handler_(ss, http_request, http_response);
+	}
+	catch (Exception& exc)
+	{
+		Tools::OutputLogger::Log_(exc.displayText());
+		return;
+	}
+}
+
+void Client::SendSSLRequest_()
+{
+	try
+	{
+        Net::initializeSSL();
+		URI uri(uri_);
+		std::string path(uri.getPathAndQuery());
+		if (path.empty()) path = "/";
+
+		Net::HTTPSClientSession session(uri.getHost(), uri.getPort(), ssl_context_);
+		Net::HTTPRequest http_request(method_, path, HTTPMessage::HTTP_1_1);
+		Net::HTTPResponse http_response;
+
+        if(use_credentials_)
+        {
+            Net::HTTPCredentials credentials(username_, password_);
+            credentials.authenticate(http_request, http_response);
+        }
+
+        session.sendRequest(http_request);
+        std::istream& is = session.receiveResponse(http_response);
+        std::stringstream ss;
+		StreamCopier::copyStream(is, ss);
+        response_handler_(ss, http_request, http_response);
+
+        Net::uninitializeSSL();
+	}
+	catch (SSLException& exc)
+	{
+		Tools::OutputLogger::Log_(exc.displayText());
+		return;
 	}
 	catch (Exception& exc)
 	{
--- src/http/client.h
+++ src/http/client.h
@@ -24,16 +24,24 @@
 #include <vector>
 #include <optional>
 
+#include "Poco/Net/HTTPSClientSession.h"
 #include "Poco/Net/HTTPClientSession.h"
 #include "Poco/Net/HTTPRequest.h"
 #include "Poco/Net/HTTPResponse.h"
 #include "Poco/Net/HTTPCredentials.h"
+#include "Poco/Net/AcceptCertificateHandler.h"
+#include <Poco/Net/Context.h>
+#include <Poco/Net/NetException.h>
+#include <Poco/Net/NetSSL.h>
+#include <Poco/Net/SSLManager.h>
 #include "Poco/StreamCopier.h"
 #include "Poco/NullStream.h"
 #include "Poco/Path.h"
 #include "Poco/URI.h"
 #include "Poco/Exception.h"
+#include <Poco/JSON/Object.h>
 
+#include "http/http_methods.h"
 #include "tools/output_logger.h"
 
 
@@ -54,22 +62,44 @@ class Atom::HTTP::Client
     public:
         using ClientResponseFunction = std::function<void(std::stringstream& response, Net::HTTPRequest& http_request, Net::HTTPResponse& http_response)>;
 
-        Client(std::string uri);
-        Client(std::string uri, std::string username, std::string password);
+        Client(std::string uri, const std::string method);
 
+        bool get_use_ssl() const { return use_ssl_; }
+        std::string get_uri() const { return uri_; }
+        std::string get_method() const { return method_; }
+        std::string get_username() const { return username_; }
+        std::string get_password() const { return password_; }
+        bool get_use_credentials() const { return use_credentials_; }
         ClientResponseFunction get_response_handler() const { return response_handler_; }
 
+        void set_use_ssl(bool use_ssl) { use_ssl_ = use_ssl; }
+        void set_uri(std::string uri) { uri_ = uri; }
+        void set_method(std::string method) { method_ = method; }
+        void set_username(std::string username) { username_ = username; }
+        void set_password(std::string password) { password_ = password; }
+        void set_use_credentials(bool use_credentials) { use_credentials_ = use_credentials; }
         void set_response_handler(ClientResponseFunction response_handler) { response_handler_ = response_handler; }
 
+        void UseCredentials_(std::string username, std::string password);
+        void SetupSSL_(std::string key, std::string cert);
+        void SetupSSL_(std::string rootcert);
+        void AddHeader_(std::string name, std::string value);
+        void AddCookie_(std::string name, std::string value);
         void SendRequest_();
-        void SendRequest_(std::string username, std::string password);
+
+    protected:
+        void SendNormalRequest_();
+        void SendSSLRequest_();
 
     private:
+        bool use_ssl_;
         std::string uri_;
+        std::string method_;
         std::string username_;
         std::string password_;
         bool use_credentials_;
         ClientResponseFunction response_handler_;
+        Context::Ptr ssl_context_;
 };
 
 #endif // ATOM_HTTP_CLIENT
