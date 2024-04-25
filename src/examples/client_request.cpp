6e44bbf22882d9bc6ab1229f968d20d2d6f93698
Jose F Rivas C <josefelixrc7@gmail.com>
2024-04-25 10:31:41 -0400

    feat: Use functions in ClientRequest example
    Use functions to add headers and cookies
    
    Rel: #130, #135

--- src/examples/client_request.cpp
+++ src/examples/client_request.cpp
@@ -1,11 +1,15 @@
 
 #include "http/client.h"
+#include "http/http_methods.h"
 
 using namespace Atom;
 
 int main()
 {
-    HTTP::Client client("https://localhost:8080");
+    HTTP::Client client("https://google.com", HTTP::HTTP_GET);
+    client.SetupSSL_("key.pem", "cert.pem");
+    client.AddHeader_("header-name", "header-value");
+    client.AddCookie_("cookie-name", "cookie-value");
     client.set_response_handler([&](std::stringstream& response, Net::HTTPRequest&, Net::HTTPResponse&)
     {
         std::cout << response.str() << std::endl;
