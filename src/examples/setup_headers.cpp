ff33ca30e1a29ec7d8ff08bed0f614d82afb3a65
Jose F Rivas C <josefelixrc7@gmail.com>
2024-04-25 10:38:10 -0400

    feat: Use functions in Setu[Headers example
    Use functions to add headers and cookies
    
    Rel: #130, #135

--- src/examples/setup_headers.cpp
+++ src/examples/setup_headers.cpp
@@ -9,16 +9,17 @@ int main(int argc, char** argv)
 {
     Core::NebulaAtom app;
 
-    app.CustomHandlerCreator_([&](HTTP::Request& request)
+    app.CustomHandlerCreator_([&](Core::BasicInfo&)
     {
         return new Handlers::CustomHandler([&](Handlers::CustomHandler& self)
         {
             // Set header
-            request.AddHeader_("cookie-example", "cookie-value");
-            request.SetupResponseHeaders_();
+            self.AddHeader_("header-example", "header-value");
+            self.AddCookie_("cookie-example", "cookie-value");
 
             // View headers
-            for(auto& header : *request.get_http_server_request().value())
+            auto headers = self.get_http_server_request().value();
+            for(auto& header : *headers)
                 std::cout << header.first << ": " << header.second << std::endl;
             
             self.CustomHTMLResponse_(HTTP::Status::kHTTP_OK, "Hello!");
