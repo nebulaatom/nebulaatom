e5d20172755cc233ace83b91cb3aa16a5ab14c49
Jose F Rivas C <josefelixrc7@gmail.com>
2024-05-01 19:59:48 -0400

    feat: Print body parameters in AvancedQuery example
    Rel: #136

--- src/examples/avanced_query.cpp
+++ src/examples/avanced_query.cpp
@@ -2,9 +2,41 @@
 #include "core/nebula_atom.h"
 #include "functions/action.h"
 #include "handlers/custom_handler.h"
+#include "http/body.h"
 #include "http/request.h"
+#include "tools/output_logger.h"
+#include <Poco/JSON/Object.h>
 
 using namespace Atom;
+
+void ViewParameters(Handlers::CustomHandler& self)
+{
+    switch(self.get_body_type())
+    {
+        case HTTP::Body::Type::kFormMultipart:
+        {
+            for(auto& file : self.get_files_parameters().get_files())
+                Tools::OutputLogger::Log_("FormMultipart parameter: " + file.get_name() + ", " + file.get_filename());
+            for(auto& value : *self.get_form())
+                Tools::OutputLogger::Log_("FormMultipart parameter: " + value.first + ", " + value.second);
+            break;
+        }
+        case HTTP::Body::Type::kJSON:
+        {
+            self.get_json_array()->stringify(std::cout);
+            self.get_json_object()->stringify(std::cout);
+            break;
+        }
+        case HTTP::Body::Type::kFormURLEncoded:
+        case HTTP::Body::Type::kURI:
+        {
+            for(auto param : self.get_query_parameters())
+                Tools::OutputLogger::Log_("URI parameter: " + param.first + ", " + param.second);
+            self.get_json_array()->stringify(std::cout);
+            break;
+        }
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -17,6 +49,13 @@ int main(int argc, char** argv)
     {
         return new Handlers::CustomHandler([&](Handlers::CustomHandler& self)
         {
+            // Process the request body
+            self.ManageRequestBody_();
+
+            // View parameters
+            ViewParameters(self);
+
+            // Create query
             Functions::Action a1("a1");
             a1.set_sql_code("SELECT * FROM products");
 
@@ -24,11 +63,11 @@ int main(int argc, char** argv)
             a1.ExecuteQuery_();
             a1.MakeResults_();
 
-            a1.get_json_result() = a1.CreateJSONResult_();
-            a1.get_json_result()->set("status", a1.get_status());
-            a1.get_json_result()->set("message", a1.get_message());
+            auto result = a1.CreateJSONResult_();
+            result->set("status", a1.get_status());
+            result->set("message", a1.get_message());
 
-            self.CompoundResponse_(HTTP::Status::kHTTP_OK, a1.get_json_result());
+            self.CompoundResponse_(HTTP::Status::kHTTP_OK, result);
         });
     });
 
