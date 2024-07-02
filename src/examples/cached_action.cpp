12efa2e9bdefa432714cd541e494e146af28bae4
Jose F Rivas C <josefelixrc7@gmail.com>
2024-07-02 12:34:52 -0400

    feat: Add cachedaction example

---
+++ src/examples/cached_action.cpp
@@ -0,0 +1,42 @@
+
+#include "core/nebula_atom.h"
+#include "functions/cache_action.h"
+#include "handlers/custom_handler.h"
+
+using namespace Atom;
+
+int main(int argc, char** argv)
+{
+    Core::NebulaAtom app;
+
+    Query::DatabaseManager::StartMySQL_();
+    Tools::SettingsManager::ReadBasicProperties_();
+
+    // Cached uery
+    Functions::CacheAction a1("a1", 5s);
+    a1.set_sql_code("SELECT * FROM products");
+    a1.set_final(true);
+
+    a1.Work_();
+
+    // Custom handler creator
+    app.CustomHandlerCreator_([&](Core::HTTPRequestInfo&)
+    {
+        return new Handlers::CustomHandler([&](Handlers::CustomHandler& self)
+        {
+            // Process the request body
+            self.ManageRequestBody_();
+
+            self.CompoundResponse_(HTTP::Status::kHTTP_OK, a1.get_json_result());
+        });
+    });
+
+    // Run
+        auto code = app.Init_(argc, argv);
+
+    // End
+        a1.set_state(false);
+        a1.get_updates_thread()->join();
+        Query::DatabaseManager::StopMySQL_();
+        return code;
+}
