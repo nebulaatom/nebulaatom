b45805e8fc8cdbf3fde84cbbaee24b05182f2503
Jose F Rivas C <josefelixrc7@gmail.com>
2024-07-09 11:51:57 -0400

    feat: Use Condition in Parameter class
    - Use Conditions to verify the parameter information
    
    Rel: #63

--- src/query/parameter.cpp
+++ src/query/parameter.cpp
@@ -1,12 +1,13 @@
 
 #include "query/parameter.h"
-#include "query/results.h"
+#include "query/condition.h"
 
 using namespace Atom::Query;
 
 Parameter::Parameter(std::string name, Tools::DValue value, bool editable) :
     parameter_type_(ParameterType::kField)
     ,name_(name)
+    ,error_("")
     ,editable_(editable)
     ,value_(value)
     ,field_position_(0, 0)
@@ -18,10 +19,16 @@ Parameter::Parameter(std::string name, Tools::DValue value, bool editable) :
 Parameter::Parameter(std::string name, Query::Field::Position field_position, std::string related_action, bool editable) :
     parameter_type_(ParameterType::kPosition)
     ,name_(name)
+    ,error_("")
     ,editable_(editable)
-    ,value_(Tools::DValue(0))
+    ,value_(Tools::DValue(""))
     ,field_position_(field_position)
     ,related_action_(related_action)
 {
+    
+}
 
+void Parameter::SetupCondition_(std::string identifier, Query::ConditionType type, Query::Condition<Ptr>::Functor functor)
+{
+    condition_ = std::make_shared<Query::Condition<Ptr>>(identifier, type, functor);
 }
--- src/query/parameter.h
+++ src/query/parameter.h
@@ -24,6 +24,7 @@
 
 #include "tools/dvalue.h"
 #include "query/results.h"
+#include "query/condition.h"
 
 
 namespace Atom
@@ -52,6 +53,7 @@ class Atom::Query::Parameter
 
         ParameterType get_parameter_type() const { return parameter_type_; }
         std::string get_name() const { return name_; }
+        std::string get_error() const { return error_; }
         bool get_editable() const { return editable_; }
         Tools::DValue& get_value()
         {
@@ -64,20 +66,30 @@ class Atom::Query::Parameter
             return var;
         }
         std::string get_related_action() const { return related_action_; }
+        Query::Condition<Ptr>::Ptr& get_condition()
+        {
+            auto& var = condition_;
+            return var;
+        }
 
         void set_parameter_type(ParameterType parameter_type) { parameter_type_ = parameter_type; }
         void set_name(std::string name) { name_ = name; }
+        void set_error(std::string error) { error_ = error; }
         void set_editable(bool editable) { editable_ = editable; }
         void set_value(Tools::DValue value) { value_ = value; }
         void set_related_action(std::string related_action) { related_action_ = related_action; }
 
+        void SetupCondition_(std::string identifier, Query::ConditionType type, Query::Condition<Ptr>::Functor functor);
+
     private:
         ParameterType parameter_type_;
         std::string name_;
+        std::string error_;
         bool editable_;
         Tools::DValue value_;
         Query::Field::Position field_position_;
         std::string related_action_;
+        Query::Condition<Ptr>::Ptr condition_;
 };
 
 #endif // ATOM_QUERY_PARAMETER
