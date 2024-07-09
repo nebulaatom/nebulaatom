e1f89605bd55b56eacfeb2725018c7d35763bb66
Jose F Rivas C <josefelixrc7@gmail.com>
2024-07-09 14:49:50 -0400

    feat: Change data member default value in Action
    Rel: #62

--- src/functions/action.cpp
+++ src/functions/action.cpp
@@ -14,7 +14,7 @@ Action::Action(std::string identifier) :
     ,status_("OK.")
     ,message_("OK.")
     ,custom_error_("")
-    ,final_(false)
+    ,final_(true)
     ,error_(false)
     ,sql_code_("SELECT 1")
     ,final_query_("")
