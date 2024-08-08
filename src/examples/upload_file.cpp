be11a0178e3ad1916a74eb652e38779f4b699130
Jose F Rivas C <josefelixrc7@gmail.com>
2024-08-08 12:58:01 -0400

    feat: Improve Uploadfile example
    Rel: #84

--- src/examples/upload_file.cpp
+++ src/examples/upload_file.cpp
@@ -2,23 +2,68 @@
 #include "core/nebula_atom.h"
 #include "files/file_manager.h"
 #include "handlers/custom_handler.h"
-#include "handlers/frontend_handler.h"
 #include "tools/dvalue.h"
 
 using namespace NAF;
 
-class UploadFile : public Handlers::FrontendHandler
+
+class Frontend : public Handlers::RootHandler
 {
     public:
-        UploadFile(){}
-        virtual ~UploadFile(){}
+        Frontend() : RootHandler(){}
 
         void Process_()
         {
-            get_file_manager().set_directory_for_uploaded_files("/var/www/uploaded-files");
-            get_file_manager().AddSupportedFile_("png", Files::FileProperties{"image/png", true, {""}});
-            UploadProcess_();
+            file_manager_.set_directory_for_uploaded_files("/var/www/uploaded-files");
+            file_manager_.AddSupportedFile_("png", Files::FileProperties{"image/png", true, {""}});
+            auto method = GetMethod_(get_properties().method);
+            switch(method)
+            {
+                case HTTP::EnumMethods::kHTTP_POST:
+                    UploadProcess_();
+                    break;
+                case HTTP::EnumMethods::kHTTP_GET:
+                case HTTP::EnumMethods::kHTTP_PUT:
+                case HTTP::EnumMethods::kHTTP_DEL:
+                case HTTP::EnumMethods::kHTTP_HEAD:
+                case HTTP::EnumMethods::kHTTP_OPTIONS:
+                case HTTP::EnumMethods::kHTTP_PATCH:
+                case HTTP::EnumMethods::kNULL:
+                    JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "The client provided a bad HTTP method.");
+                    break;
+            }
         }
+
+        void UploadProcess_()
+        {
+            // Manage the files
+                auto& request = get_http_server_request().value();
+                file_manager_.set_operation_type(Files::OperationType::kUpload);
+                HTMLForm form(*request, request->stream(), file_manager_);
+
+            // Verify supported files
+                if(!file_manager_.IsSupported_())
+                {
+                    HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Requested file is not supported.");
+                    return;
+                }
+
+            // Verify max file size
+                if(!file_manager_.VerifyMaxFileSize_())
+                {
+                    HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "The requested file exceeds the file size limit.");
+                    return;
+                }
+
+            // Upload file
+                file_manager_.UploadFile_();
+
+            // Response
+                CompoundFillResponse_(HTTP::Status::kHTTP_OK, file_manager_.get_result(), "Ok.");
+        }
+
+    private:
+        Files::FileManager file_manager_;
 };
 
 int main(int argc, char** argv)
@@ -48,7 +93,7 @@ int main(int argc, char** argv)
 
     app.AddHandler_("/upload", [&]()
     {
-        return new UploadFile;
+        return new Frontend;
     });
 
     app.AddHandler_("/database", [&]()
