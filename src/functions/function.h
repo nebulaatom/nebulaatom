71132e7f428e1183b9ca223776a23af0b68deb7a
Jose F Rivas C <josefelixrc7@gmail.com>
2024-08-09 18:52:10 -0400

    feat: Add ResponseType to Function class
    Rel: #86

--- src/functions/function.h
+++ src/functions/function.h
@@ -27,6 +27,12 @@ class NAF::Functions::Function
     public:
         using Ptr = std::shared_ptr<Functions::Function>;
 
+        enum class ResponseType
+        {
+            kJSON
+            ,kFile
+        };
+
         Function();
         Function(std::string endpoint, HTTP::EnumMethods type);
 
@@ -34,6 +40,7 @@ class NAF::Functions::Function
         std::string get_target() const { return target_; }
         bool get_error() const { return error_; }
         std::string get_error_message() const { return error_message_; }
+        ResponseType get_response_type() const { return response_type_; }
         HTTP::EnumMethods get_method() const { return method_; }
         std::vector<Action::Ptr>& get_actions()
         {
@@ -49,6 +56,7 @@ class NAF::Functions::Function
         void set_endpoint(std::string endpoint) { endpoint_ = endpoint; }
         void set_target(std::string target) { target_ = target; }
         void set_method(HTTP::EnumMethods type) { method_ = type; }
+        void set_response_type(ResponseType response_type) { response_type_ = response_type; }
 
         Action::Ptr AddAction_(std::string identifier);
         bool ProcessJSONResponse_(JSON::Object::Ptr& json_result);
@@ -58,6 +66,7 @@ class NAF::Functions::Function
         std::string target_;
         bool error_;
         std::string error_message_;
+        ResponseType response_type_;
         HTTP::EnumMethods method_;
         std::vector<Action::Ptr> actions_;
         HTTP::Methods methods_;
