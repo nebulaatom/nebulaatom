9e59caa1162fb971f11c7ecd71283cb82c45d899
Jose F Rivas C <josefelixrc7@gmail.com>
2024-08-11 15:32:24 -0400

    feat: Improve Frontend example
    Rel: #86, #87

--- src/examples/frontend.cpp
+++ src/examples/frontend.cpp
@@ -1,6 +1,7 @@
 
 #include "core/nebula_atom.h"
 #include "functions/action.h"
+#include "functions/function.h"
 #include "handlers/custom_handler.h"
 #include "handlers/root_handler.h"
 #include "tools/route.h"
@@ -9,181 +10,130 @@
 using namespace NAF;
 using namespace NAF::Handlers;
 
-class Frontend : public Handlers::RootHandler
+class MainHandler : public Handlers::BackendHandler
 {
     public:
-        Frontend() : RootHandler(){}
+        MainHandler() : Handlers::BackendHandler(){}
+        virtual ~MainHandler() {}
 
-        void Process_()
+        void AddFunctions_()
         {
-            file_manager_.set_directory_base("/var/www");
-            file_manager_.set_directory_for_uploaded_files("/var/www/uploaded-files");
-            file_manager_.AddBasicSupportedFiles_();
-            auto method = GetMethod_(get_properties().method);
-            switch(method)
-            {
-                case HTTP::EnumMethods::kHTTP_GET:
-                    DownloadProcess_();
-                    break;
-                case HTTP::EnumMethods::kHTTP_POST:
-                    UploadProcess_();
-                    break;
-                case HTTP::EnumMethods::kHTTP_PUT:
-                    ModifyProcess_();
-                    break;
-                case HTTP::EnumMethods::kHTTP_DEL:
-                    RemoveProcess_();
-                    break;
-                case HTTP::EnumMethods::kHTTP_HEAD:
-                case HTTP::EnumMethods::kHTTP_OPTIONS:
-                case HTTP::EnumMethods::kHTTP_PATCH:
-                case HTTP::EnumMethods::kNULL:
-                    JSONResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "The client provided a bad HTTP method.");
-                    break;
-            }
+            // Function /api/file/read
+                auto f1 = AddFunction_("/api/file/read", HTTP::EnumMethods::kHTTP_GET);
+                f1->set_response_type(Functions::Function::ResponseType::kFile);
+
+                f1->get_file_manager()->set_directory_base("/var/www/uploaded-files");
+                f1->get_file_manager()->AddBasicSupportedFiles_();
+
+                // Action 1
+                auto a1 = f1->AddAction_("");
+                a1->set_custom_error("No image found.");
+                a1->set_sql_code("SELECT '2024811_18502548_1611731744_aire_acondicionado_tamaños.png'");
+
+            // Function /api/file/add
+                auto f2 = AddFunction_("/api/file/add", HTTP::EnumMethods::kHTTP_POST);
+                f2->set_response_type(Functions::Function::ResponseType::kFile);
+
+                f2->get_file_manager()->set_directory_base("/var/www/uploaded-files");
+                f2->get_file_manager()->AddSupportedFile_("png", Files::FileProperties{"image/png", true, {""}});
+
+            // Function /api/file/modify
+                auto f3 = AddFunction_("/api/file/modify", HTTP::EnumMethods::kHTTP_PUT);
+                f3->set_response_type(Functions::Function::ResponseType::kFile);
+
+                f3->get_file_manager()->set_directory_base("/var/www/uploaded-files");
+                f3->get_file_manager()->AddBasicSupportedFiles_();
+
+                // Action 1
+                auto a3 = f3->AddAction_("");
+                a3->set_custom_error("No image found.");
+                a3->set_sql_code("SELECT '20160315_v210.png'");
+
+            // Function /api/file/remove
+                auto f4 = AddFunction_("/api/file/remove", HTTP::EnumMethods::kHTTP_DEL);
+                f4->set_response_type(Functions::Function::ResponseType::kFile);
+
+                f4->get_file_manager()->set_directory_base("/var/www/uploaded-files");
+                f4->get_file_manager()->AddBasicSupportedFiles_();
+
+                // Action 1
+                auto a4 = f4->AddAction_("");
+                a4->set_custom_error("No image found.");
+                a4->set_sql_code("SELECT '2024811_18502548_1611731744_aire_acondicionado_tamaños.png'");
         }
 
-        void DownloadProcess_()
+        void Process_() override
         {
-            // Manage the file
+            AddFunctions_();
 
-                auto& request = get_http_server_request().value();
-                file_manager_.set_operation_type(Files::OperationType::kDownload);
-                file_manager_.get_files().push_back(file_manager_.CreateTempFile_(request->getURI()));
-                auto& tmp_file = file_manager_.get_files().front();
-
-            // Check file
-                if(!file_manager_.CheckFiles_())
-                {
-                    HTMLResponse_(HTTP::Status::kHTTP_NOT_FOUND, "Requested file bad check.");
-                    return;
-                }
-
-            // Is supported
-                if(!file_manager_.IsSupported_(tmp_file))
-                {
-                    HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Requested file is not supported.");
-                    return;
-                }
-                file_manager_.ProcessContentLength_();
-
-            // Reponse
-                auto& response = get_http_server_response().value();
-                response->setStatus(HTTPResponse::HTTP_OK);
-                response->setContentType(tmp_file.get_content_type());
-                response->setContentLength(tmp_file.get_content_length());
+            // Set security type
+                set_security_type(Extras::SecurityType::kDisableAll);
                 
-                std::ostream& out_reponse = response->send();
+            // Process the request body
+                ManageRequestBody_();
 
-            // Download file
-                file_manager_.DownloadFile_(out_reponse);
+            // Route identification
+                if(!IdentifyRoute_())
+                {
+                    JSONResponse_(HTTP::Status::kHTTP_NOT_FOUND, "The requested endpoint ("+get_properties().method+") is not available.");
+                    return;
+                }
+
+            // Process actions
+                ProcessActions_();
         }
-
-        void UploadProcess_()
-        {
-            // Manage the files
-                auto& request = get_http_server_request().value();
-                file_manager_.set_operation_type(Files::OperationType::kUpload);
-                HTMLForm form(*request, request->stream(), file_manager_);
-
-            // Verify supported files
-                if(!file_manager_.IsSupported_())
-                {
-                    HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Requested file is not supported.");
-                    return;
-                }
-
-            // Verify max file size
-                if(!file_manager_.VerifyMaxFileSize_())
-                {
-                    HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "The requested file exceeds the file size limit.");
-                    return;
-                }
-
-            // Upload file
-                file_manager_.UploadFile_();
-
-            // Response
-                CompoundFillResponse_(HTTP::Status::kHTTP_OK, file_manager_.get_result(), "Ok.");
-        }
-
-        void ModifyProcess_()
-        {
-            // Manage the file
-                auto& request = get_http_server_request().value();
-                file_manager_.set_operation_type(Files::OperationType::kDelete);
-                file_manager_.get_files().push_back(file_manager_.CreateTempFile_(request->getURI()));
-
-            // Check file
-                if(!file_manager_.CheckFiles_())
-                {
-                    HTMLResponse_(HTTP::Status::kHTTP_NOT_FOUND, "Requested file bad check.");
-                    return;
-                }
-
-            // Remove the file
-                file_manager_.RemoveFile_();
-                file_manager_.get_files().clear();
-                file_manager_.get_result()->clear();
-
-            // Process new file to upload
-                file_manager_.set_operation_type(Files::OperationType::kUpload);
-                HTMLForm form(*request, request->stream(), file_manager_);
-
-                if(!file_manager_.IsSupported_())
-                {
-                    HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "Requested file is not supported.");
-                    return;
-                }
-                
-            // Verify max file size
-                if(!file_manager_.VerifyMaxFileSize_())
-                {
-                    HTMLResponse_(HTTP::Status::kHTTP_BAD_REQUEST, "The requested file exceeds the file size limit.");
-                    return;
-                }
-
-            // Upload
-                file_manager_.UploadFile_();
-
-            // Response
-                CompoundFillResponse_(HTTP::Status::kHTTP_OK, file_manager_.get_result(), "Ok.");
-        }
-
-        void RemoveProcess_()
-        {
-            // Manage file
-                auto& request = get_http_server_request().value();
-                file_manager_.set_operation_type(Files::OperationType::kDelete);
-                file_manager_.get_files().push_back(file_manager_.CreateTempFile_(request->getURI()));
-
-            // Basic operations
-                if(!file_manager_.CheckFiles_())
-                {
-                    HTMLResponse_(HTTP::Status::kHTTP_NOT_FOUND, "Requested file bad check.");
-                    return;
-                }
-
-            // Remove
-                file_manager_.RemoveFile_();
-                
-            // Response
-                JSONResponse_(HTTP::Status::kHTTP_OK, "Ok.");
-        }
-
-    private:
-        Files::FileManager file_manager_;
 };
 
 int main(int argc, char** argv)
 {
     Core::NebulaAtom app;
 
+    Query::DatabaseManager::StartMySQL_();
     Tools::SettingsManager::ReadBasicProperties_();
 
-    app.CustomHandlerCreator_([&](Core::HTTPRequestInfo&)
+    app.CustomHandlerCreator_([&](Core::HTTPRequestInfo& info)
     {
-        return new Frontend();
+            // Routes
+            NAF::Tools::Route requested_route(info.uri);
+            NAF::Tools::Route upload("/upload");
+            NAF::Tools::Route modify("/modify");
+
+            // Return handler
+            Handlers::RootHandler* handler;
+            if(requested_route == upload)
+                handler = new Handlers::CustomHandler([&](Handlers::CustomHandler& self)
+                {
+                    self.CustomHTMLResponse_(HTTP::Status::kHTTP_OK, std::string(" \
+                        <html><head><title>Upload file example</title></head> \
+                        <body><input type=\"file\" id=\"image-file\"><button onclick=\"upload()\">Upload</button><script> \
+                            function upload(){ \
+                                let photo = document.getElementById(\"image-file\").files[0]; \
+                                let formData = new FormData(); \
+                                formData.append(\"photo\", photo); \
+                                fetch('/api/file/add', {method: \"POST\", body: formData}); \
+                            } \
+                        </script></body> \
+                    "));
+                });
+            else if(requested_route == modify)
+                handler = new Handlers::CustomHandler([&](Handlers::CustomHandler& self)
+                {
+                    self.CustomHTMLResponse_(HTTP::Status::kHTTP_OK, std::string(" \
+                        <html><head><title>Modify file example</title></head> \
+                        <body><input type=\"file\" id=\"image-file\"><button onclick=\"modify()\">Upload</button><script> \
+                            function modify(){ \
+                                let photo = document.getElementById(\"image-file\").files[0]; \
+                                let formData = new FormData(); \
+                                formData.append(\"photo\", photo); \
+                                fetch('/api/file/modify', {method: \"PUT\", body: formData}); \
+                            } \
+                        </script></body> \
+                    "));
+                });
+            else
+                handler = new MainHandler;
+
+            return handler;
     });
 
     return app.Init_(argc, argv);
