0fe528ed6ba83e79008b8d9f12a123c18b15b753
Jose F Rivas C <josefelixrc7@gmail.com>
2024-08-13 17:05:26 -0400

    feat: Add functions  to Read Settings
    Rel: #83

--- src/tools/settings_manager.cpp
+++ src/tools/settings_manager.cpp
@@ -22,8 +22,8 @@ using namespace NAF;
 using namespace Tools;
 
 std::mutex SettingsManager::mutex_;
-Tools::SettingsManager::BasicProperties Tools::SettingsManager::basic_properties_ = {};
-std::string Tools::SettingsManager::properties_file_address_ = "properties.yaml";
+std::vector<Tools::SettingsManager::Setting> Tools::SettingsManager::settings_ = {};
+std::string Tools::SettingsManager::config_path_ = "properties.yaml";
 
 Tools::SettingsManager::SettingsManager()
 {
@@ -99,141 +99,61 @@ void Tools::SettingsManager::AddSetting_(std::string name, Tools::DValue::Type t
     settings_.push_back(Setting(name, type, value));
 }
 
-void Tools::SettingsManager::ReadBasicProperties_()
+void Tools::SettingsManager::ReadSettings_()
 {
     try
     {
         mutex_.lock();
         // Read YAML functions file
-        YAML::Node config = YAML::LoadFile(properties_file_address_);
+        YAML::Node config = YAML::LoadFile(config_path_);
 
-        // port
-        auto port = config["port"];
-        if (!VerifyYAMLScalarNode_(port))
-            PrintError_("ReadBasicProperties_", "port");
+        for(auto& setting : settings_)
+        {
+            try
+            {
+                auto tmp_val = config[setting.name];
+                if (!VerifyYAMLScalarNode_(tmp_val))
+                    continue;
 
-        basic_properties_.port = port.as<int>();
-        
-        // max_queued
-        auto max_queued = config["max_queued"];
-        if (!VerifyYAMLScalarNode_(max_queued))
-            PrintError_("ReadBasicProperties_", "max_queued");
-            
-        basic_properties_.max_queued = max_queued.as<int>();
-        
-        // max_threads
-        auto max_threads = config["max_threads"];
-        if (!VerifyYAMLScalarNode_(max_threads))
-            PrintError_("ReadBasicProperties_", "max_threads");
-            
-        basic_properties_.max_threads = max_threads.as<int>();
-        
-        // session_max_age
-        auto session_max_age = config["session_max_age"];
-        if (!VerifyYAMLScalarNode_(session_max_age))
-            PrintError_("ReadBasicProperties_", "session_max_age");
-            
-        basic_properties_.session_max_age = session_max_age.as<int>();
-        
-        // max_file_size
-        auto max_file_size = config["max_file_size"];
-        if (!VerifyYAMLScalarNode_(max_file_size))
-            PrintError_("ReadBasicProperties_", "max_file_size");
-            
-        basic_properties_.max_file_size = max_file_size.as<float>();
-        
-        // directory_base
-        auto directory_base = config["directory_base"];
-        if (!VerifyYAMLScalarNode_(directory_base))
-            PrintError_("ReadBasicProperties_", "directory_base");
+                switch(setting.type)
+                {
+                    case Tools::DValue::Type::kEmpty:
+                    {
+                        break;
+                    }
+                    case Tools::DValue::Type::kBoolean:
+                    {
+                        setting.value = Tools::DValue(tmp_val.as<bool>());
+                        break;
+                    }
+                    case Tools::DValue::Type::kFloat:
+                    {
+                        setting.value = Tools::DValue(tmp_val.as<float>());
+                        break;
+                    }
+                    case Tools::DValue::Type::kInteger:
+                    {
+                        setting.value = Tools::DValue(tmp_val.as<int>());
+                        break;
+                    }
+                    case Tools::DValue::Type::kString:
+                    {
+                        setting.value = Tools::DValue(tmp_val.as<std::string>());
+                        break;
+                    }
+                }
+            }
+            catch(std::exception& e)
+            {
+                PrintError_("ReadSettings_", setting.name + ": " + e.what());
+            }
+        }
 
-        basic_properties_.directory_base = directory_base.as<std::string>();
-        
-        // directory_for_temp_files
-        auto directory_for_temp_files = config["directory_for_temp_files"];
-        if (!VerifyYAMLScalarNode_(directory_for_temp_files))
-            PrintError_("ReadBasicProperties_", "directory_for_temp_files");
-            
-        basic_properties_.directory_for_temp_files = directory_for_temp_files.as<std::string>();
-        
-        // db_host
-        auto db_host = config["db_host"];
-        if (!VerifyYAMLScalarNode_(db_host))
-            PrintError_("ReadBasicProperties_", "db_host");
-
-        basic_properties_.db_host = db_host.as<std::string>();
-        
-        // db_port
-        auto db_port = config["db_port"];
-        if (!VerifyYAMLScalarNode_(db_port))
-            PrintError_("ReadBasicProperties_", "db_port");
-            
-        basic_properties_.db_port = db_port.as<std::string>();
-        
-        // db_name
-        auto db_name = config["db_name"];
-        if (!VerifyYAMLScalarNode_(db_name))
-            PrintError_("ReadBasicProperties_", "db_name");
-            
-        basic_properties_.db_name = db_name.as<std::string>();
-        
-        // db_user
-        auto db_user = config["db_user"];
-        if (!VerifyYAMLScalarNode_(db_user))
-            PrintError_("ReadBasicProperties_", "db_user");
-            
-        basic_properties_.db_user = db_user.as<std::string>();
-        
-        // db_password
-        auto db_password = config["db_password"];
-        if (!VerifyYAMLScalarNode_(db_password))
-            PrintError_("ReadBasicProperties_", "db_password");
-            
-        basic_properties_.db_password = db_password.as<std::string>();
-        
-        // certificate
-        auto certificate = config["certificate"];
-        if (!VerifyYAMLScalarNode_(certificate))
-            PrintError_("ReadBasicProperties_", "certificate");
-            
-        basic_properties_.certificate = certificate.as<std::string>();
-        
-        // key
-        auto key = config["key"];
-        if (!VerifyYAMLScalarNode_(key))
-            PrintError_("ReadBasicProperties_", "key");
-            
-        basic_properties_.key = key.as<std::string>();
-        
-        // rootcert
-        auto rootcert = config["rootcert"];
-        if (!VerifyYAMLScalarNode_(rootcert))
-            PrintError_("ReadBasicProperties_", "rootcert");
-            
-        basic_properties_.rootcert = rootcert.as<std::string>();
-        
-        // logger_output_file_
-        auto logger_output_file = config["logger_output_file"];
-        if (!VerifyYAMLScalarNode_(logger_output_file))
-            PrintError_("ReadBasicProperties_", "logger_output_file");
-        
-        basic_properties_.logger_output_file = logger_output_file.as<std::string>();
-        Tools::OutputLogger::set_output_file_address(basic_properties_.logger_output_file);
-        
-        // debug
-        auto debug = config["debug"];
-        if (!VerifyYAMLScalarNode_(debug))
-            PrintError_("ReadBasicProperties_", "debug");
-        
-        basic_properties_.debug = debug.as<bool>();
-        Tools::OutputLogger::set_print_debug(basic_properties_.debug);
-        
-        
         mutex_.unlock();
     }
     catch(std::exception& e)
     {
-        PrintError_("ReadBasicProperties_", std::string(e.what()));
+        PrintError_("ReadSettings_", std::string(e.what()));
         return;
     }
 }
