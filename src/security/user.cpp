607e53733d7fcbd990a18cf46cbe961a457d63f0
Jose F Rivas C <josefelixrc7@gmail.com>
2024-09-13 14:21:00 -0400

    feat: Add group id to User class
    Rel: #91

--- src/security/user.cpp
+++ src/security/user.cpp
@@ -23,13 +23,15 @@ using namespace NAF::Security;
 User::User() :
     id_(-1)
     ,username_("")
+    ,id_group_(-1)
 {
 
 }
 
-User::User(int id, std::string username) :
+User::User(int id, std::string username, int id_group) :
     id_(id)
     ,username_(username)
+    ,id_group_(id_group)
 {
 
 }
--- src/security/user.h
+++ src/security/user.h
@@ -37,17 +37,20 @@ class NAF::Security::User
 {
     public:
         User();
-        User(int id, std::string username);
+        User(int id, std::string username, int id_group);
 
         int get_id() const { return id_; }
         std::string get_username() const { return username_; }
+        int get_id_group() const { return id_group_; }
 
         void set_id(int id) { id_ = id; }
         void set_username(std::string username) { username_ = username; }
+        void set_id_group(int id_group) { id_group_ = id_group; }
 
     private:
         int id_;
         std::string username_;
+        int id_group_;
 };
 
 
